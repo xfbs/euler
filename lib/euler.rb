30d8432b1e5ec482548927fc5eeaebddfc1dcde4
Patrick Elsen <pelsen@xfbs.net>
2017-07-19 02:44:33 +0200

    adds preliminary Euler::DB class - wip

--- lib/euler.rb
+++ lib/euler.rb
@@ -5,21 +5,5 @@ require "euler/solutions"
 require 'yaml'
 
 module Euler
-  module Languages
-    def self.load file
-      f = File.open(file, 'r')
-      langs = YAML.load(f.read)
-      f.close
-
-      raise "Error loading languages" unless langs && langs.class == Hash
-
-      langs.each do |ext, lang|
-        lang['ext'] = ext
-        lang['name'] = lang['name'] || ext
-      end
-
-      langs
-    end
-  end
   # Your code goes here...
 end
--- lib/euler/db.rb
+++ lib/euler/db.rb
@@ -1,8 +1,38 @@
 require 'yaml'
 
+# note: for dumping the data to yaml, might have a look at
+# http://www.rubydoc.info/gems/psych/Psych#to_json-class_method
+# as well as
+# http://yaml4r.sourceforge.net/doc/page/type_families.htm
+# for coming up with some nicer ways to dump these :)
+
+# note: do we need a language class?
+
 module Euler
   class DB
-    def initialize
+    def initialize file
+      @db_file = file
     end
+
+    def read
+      raise "db read already" if @db
+
+      # read in database
+      File.open(file, 'r') do |fh|
+        # parse database
+        @db = YAML.load(db)
+      end
+
+      @db['langs'] ||= {}
+      @db['probs'] ||= {}
+
+      true
+    end
+
+    def language lang={}
+      return @db['langs'] if @db['langs'][lang]
+    end
+
+
   end
 end
