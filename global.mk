316f77c2efc3d192522c35a1b4a96f149956f861
Patrick Elsen <pelsen@xfbs.net>
2017-11-27 21:17:37 +0100

    declutters files

--- global.mk
+++
@@ -1,6 +0,0 @@
-ROOTDIR		= $(dir $(realpath $(lastword $(MAKEFILE_LIST))))
-DATADIR		= $(ROOTDIR)data
-CHECKERDIR	= $(ROOTDIR)checker
-CACHEDIR	= $(ROOTDIR).cache
-PROBLEMDIR 	= $(ROOTDIR)problems
-SOLUTIONDIR	= $(ROOTDIR)solutions
