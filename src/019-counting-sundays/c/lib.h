c0292e7626b2204991d79f21c704a224de9333a3
Patrick Elsen <pelsen@xfbs.net>
2017-11-30 16:16:08 +0100

    adds solution in c

---
+++ src/019-counting-sundays/c/lib.h
@@ -0,0 +1,46 @@
+#include <stdbool.h>
+#include <string.h>
+#include <stdint.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <math.h>
+#pragma once
+
+typedef enum {
+    MONDAY,
+    TUESDAY,
+    WEDNESDAY,
+    THURSDAY,
+    FRIDAY,
+    SATURDAY,
+    SUNDAY,
+    WEEKDAY_MAX
+} weekday_t;
+
+typedef enum {
+    JANUARY,
+    FEBRUARY,
+    MARCH,
+    APRIL,
+    MAY,
+    JUNE,
+    JULY,
+    AUGUST,
+    SEPTEMBER,
+    OCTOBER,
+    NOVEMBER,
+    DECEMBER,
+    MONTH_MAX
+} month_t;
+
+typedef struct {
+    weekday_t weekday;
+    int day;
+    month_t month;
+    int year;
+} date_t;
+
+uint32_t solve(date_t start, date_t end);
+bool is_leap_year(date_t date);
+size_t month_length(date_t date);
+date_t next_month(date_t date);
