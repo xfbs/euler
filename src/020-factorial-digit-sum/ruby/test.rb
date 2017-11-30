29fae0e383135ac863c63ff116a7db95ef74a6e1
Patrick Elsen <pelsen@xfbs.net>
2017-11-30 14:36:13 +0100

    adds solution to problem 20 in ruby

---
+++ src/020-factorial-digit-sum/ruby/test.rb
@@ -0,0 +1,29 @@
+#!/usr/bin/env ruby
+require_relative 'solution'
+require 'minitest'
+
+class SolutionTest < MiniTest::Test
+  def setup
+  end
+
+  def test_factorial_factorised
+    assert_equal Solution.factorial_factorised(2), {2=>1}
+    assert_equal Solution.factorial_factorised(3), {2=>1, 3=>1}
+    assert_equal Solution.factorial_factorised(4), {2=>3, 3=>1}
+    assert_equal Solution.factorial_factorised(5), {2=>3, 3=>1, 5=>1}
+    assert_equal Solution.factorial_factorised(6), {2=>4, 3=>2, 5=>1}
+  end
+
+  def test_factorial_trimmed
+    assert_equal Solution.factorial_trimmed(2), 2
+    assert_equal Solution.factorial_trimmed(3), 6
+    assert_equal Solution.factorial_trimmed(4), 24
+    assert_equal Solution.factorial_trimmed(10), 36288
+  end
+
+  def test_solution
+    assert_equal Solution.solve(10), 27
+  end
+end
+
+MiniTest.autorun
