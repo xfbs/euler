fd29430c2dca6d820c9149809957e62727b620bd
Patrick Elsen <pelsen@xfbs.net>
2017-12-03 09:34:35 +0100

    adds solution in ruby

---
+++ src/031-coin-sums/ruby/test.rb
@@ -0,0 +1,17 @@
+#!/usr/bin/env ruby
+require_relative 'solution'
+require 'minitest'
+
+class SolutionTest < MiniTest::Test
+  def setup
+  end
+
+  def test_change
+    assert_equal 1, Solution.change(coins: [1], value: 1)
+    assert_equal 2, Solution.change(coins: [2, 1], value: 3)
+    assert_equal 4, Solution.change(coins: [5, 2, 1], value: 5)
+    assert_equal 5, Solution.change(coins: [5, 2, 1], value: 6)
+  end
+end
+
+MiniTest.autorun
