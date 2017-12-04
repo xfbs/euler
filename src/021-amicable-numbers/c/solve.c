2ba2e76925ca4d3935729afa901c54745844c773
Patrick Elsen <pelsen@xfbs.net>
2017-12-04 22:48:40 +0100

    [021/c] fixes divisor_sum function

--- src/021-amicable-numbers/c/solve.c
+++ src/021-amicable-numbers/c/solve.c
@@ -39,7 +39,7 @@ uint32_t divisor_sum(uint32_t num)
 {
     uint32_t sum = 0;
 
-    for(uint32_t i = 1; i < (uint32_t)(0.5 + sqrt(num)); i++) {
+    for(uint32_t i = 1; i <= (uint32_t)sqrt(num); i++) {
         if((num % i) == 0) {
             sum += i;
             if(i != (num / i) && i != 1) {
