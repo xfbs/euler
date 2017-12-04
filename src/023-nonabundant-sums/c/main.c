dfe7e619e90d86bc82885f7b6afa1cb604edae4c
Patrick Elsen <pelsen@xfbs.net>
2017-12-04 22:48:52 +0100

    [021/c] adds solution

---
+++ src/023-nonabundant-sums/c/main.c
@@ -0,0 +1,9 @@
+#include "solve.h"
+#include <stdio.h>
+
+int main(int argc, char *argv[])
+{
+    printf("%i\n", solve(28123));
+
+    return 0;
+}
---
+++ src/023-nonabundant-sums/c/solve.c
@@ -0,0 +1,49 @@
+#include "solve.h"
+
+uint32_t solve(size_t max)
+{
+    bitvec_t poss = bitvec_new(max);
+    uint32_t abundant[max];
+    size_t pos = 0;
+
+    for(size_t i = 1; i < max; i++) {
+        if(divisor_sum(i) > i) {
+            abundant[pos] = i;
+            pos++;
+        }
+    }
+
+    for(size_t a = 0; a < pos; a++) {
+        for(size_t b = a; b < pos; b++) {
+            uint32_t sum = abundant[a] + abundant[b];
+            if(sum < max) {
+                bitvec_set(&poss, sum);
+            }
+        }
+    }
+
+    uint32_t sum = 0;
+    for(size_t i = 0; i < max; i++) {
+        if(!bitvec_get(&poss, i)) {
+            sum += i;
+        }
+    }
+
+    return sum;
+}
+
+uint32_t divisor_sum(uint32_t num)
+{
+    uint32_t sum = 0;
+
+    for(uint32_t i = 1; i <= (uint32_t)sqrt(num); i++) {
+        if((num % i) == 0) {
+            sum += i;
+            if(i != (num / i) && i != 1) {
+                sum += (num / i);
+            }
+        }
+    }
+
+    return sum;
+}
