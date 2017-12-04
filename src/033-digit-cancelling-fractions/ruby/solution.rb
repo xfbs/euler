b966c31784b2bd2d7334f969ea49f1fb7d0f4a16
Patrick Elsen <pelsen@xfbs.net>
2017-12-04 23:58:32 +0100

    [033/ruby] adds solution

---
+++ src/033-digit-cancelling-fractions/ruby/solution.rb
@@ -0,0 +1,30 @@
+module Solution
+  def self.solve
+    fractions = []
+
+    (0..9).each do |a|
+      (0..9).each do |b|
+        (0..9).each do |c|
+          (0..9).each do |d|
+            if check_fraction(a, b, c, d)
+              fractions << [10 * a + b, 10 * c + d]
+            end
+          end
+        end
+      end
+    end
+
+    fractions = fractions.inject([1,1]){|a, b| [a[0]*b[0], a[1]*b[1]]}
+    fractions[1] / fractions[0].gcd(fractions[1])
+  end
+
+  def self.check_fraction a, b, c, d
+    frac = (10.0 * a + b) / (10.0 * c + d)
+    return false if frac > 1.0
+    return false if frac == 0
+
+    return true if b == c && frac == (a.to_f / d.to_f)
+    return true if a == d && frac == (b.to_f / c.to_f)
+    false
+  end
+end
