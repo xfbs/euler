ca0d1e96608127c43c5552c92db386273cdc2ac3
Patrick Elsen <pelsen@xfbs.net>
2017-12-04 12:50:49 +0100

    simplifies python solution

--- src/025-1000digit-fibonacci-number/python/test.py
+++ src/025-1000digit-fibonacci-number/python/test.py
@@ -1,9 +1,9 @@
 import unittest
-import solution
+import solve
 
 class TestFibonacci(unittest.TestCase):
    def test_fibonacci(self):
-      fibo = solution.Fibonacci()
+      fibo = solve.Fibonacci()
       self.assertEqual(fibo.next(), 2)
       self.assertEqual(fibo.next(), 3)
       self.assertEqual(fibo.next(), 5)
@@ -11,7 +11,7 @@ class TestFibonacci(unittest.TestCase):
 class TestSolution(unittest.TestCase):
     # test the solution function
     def test_solution(self):
-        self.assertEqual(solution.solve(3), 12)
+        self.assertEqual(solve.solve(3), 12)
 
 if __name__ == '__main__':
     unittest.main()
