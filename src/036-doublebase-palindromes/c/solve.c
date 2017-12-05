7973503927d6fb8568767deee908c43cd499105f
Patrick Elsen <pelsen@xfbs.net>
2017-12-05 10:48:31 +0100

    [036/c] adds solution

---
+++ src/036-doublebase-palindromes/c/solve.c
@@ -0,0 +1,40 @@
+#include "solve.h"
+#include <stdio.h>
+
+uint32_t solve(size_t max)
+{
+    uint32_t sum = 0;
+
+    for(uint32_t i = 0; i < max; i++) {
+        if(is_palindrome(i, 2) && is_palindrome(i, 10)) {
+            sum += i;
+        }
+    }
+
+    return sum;
+}
+
+bool is_palindrome(uint32_t num, size_t base)
+{
+    uint32_t mag = 1;
+    uint32_t max = base;
+
+    while(num >= max) {
+        mag += 1;
+        max *= base;
+    }
+
+    while(mag > 1) {
+        mag -= 2;
+        uint32_t right = num % base;
+        num /= base;
+        max /= base;
+        max /= base;
+        uint32_t left = num / max;
+        if(right != left) return false;
+        num -= left * max;
+    }
+
+    return true;
+}
+
---
+++ src/036-doublebase-palindromes/c/solve.h
@@ -0,0 +1,7 @@
+#include <stdint.h>
+#include <stddef.h>
+#include <stdbool.h>
+#pragma once
+
+uint32_t solve(size_t max);
+bool is_palindrome(uint32_t num, size_t base);
