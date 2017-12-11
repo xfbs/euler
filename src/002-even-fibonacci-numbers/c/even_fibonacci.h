2601e0591a880a8b3a79ff87b24632ab12cbff68
Patrick Elsen <pelsen@xfbs.net>
2017-12-11 12:05:43 +0100

    [002/c] fixes phi constant declaration

--- src/002-even-fibonacci-numbers/c/even_fibonacci.h
+++ src/002-even-fibonacci-numbers/c/even_fibonacci.h
@@ -2,7 +2,7 @@
 #include <math.h>
 #pragma once
 
-static const double PHI = 1.61803398875;
-static const double PHI3 = PHI * PHI * PHI;
+static double PHI = 1.61803398875;
+static double PHI3 = PHI * PHI * PHI;
 
 uint64_t next_even_fibonacci(uint64_t fibo);
