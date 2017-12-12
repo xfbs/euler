258b7310dd387c5f9c12e39a02fc48f4edd231d0
Patrick Elsen <pelsen@xfbs.net>
2017-12-12 11:57:57 +0100

    [046/c] adds solution

---
+++ src/046-goldbachs-other-conjecture/c/solve.c
@@ -0,0 +1,37 @@
+#include <stddef.h>
+#include <stdbool.h>
+#include "solve.h"
+
+uint64_t solve()
+{
+    // gen some primes
+    prime_t p = prime_new();
+    prime_nth(&p, 100000);
+
+    uint64_t num = 1;
+    while(true) {
+        // next odd number
+        num += 2;
+
+        // make sure number is composite
+        while(SIZE_MAX != prime_which(&p, num)) num += 2;
+
+        // use this to keep track if the conjecture holds for num or not
+        bool conjecture = false;
+
+        // we start our search at the 2nd prime, because the conjecture only
+        // works for odd primes (since twice any square will always be even)
+        for(size_t i = 1; prime_nth(&p, i) < num; i++) {
+            uint64_t square = num - prime_nth(&p, i);
+            square /= 2;
+            uint64_t root = sqrt(square);
+
+            if((root * root) == square) {
+                conjecture = true;
+                break;
+            }
+        }
+
+        if(!conjecture) return num;
+    }
+}
---
+++ src/046-goldbachs-other-conjecture/c/solve.h
@@ -0,0 +1,6 @@
+#include <stdint.h>
+#include <simple_prime.h>
+#include <math.h>
+#pragma once
+
+uint64_t solve();
