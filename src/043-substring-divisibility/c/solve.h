c0b1230c5f22444ac2796a8f9ca468ed375b25ef
Patrick Elsen <pelsen@xfbs.net>
2017-12-12 10:51:53 +0100

    [043/c] adds solution

---
+++ src/043-substring-divisibility/c/solve.h
@@ -0,0 +1,5 @@
+#include <stdint.h>
+#pragma once
+
+uint64_t solve();
+uint64_t pandigital(uint32_t d2d3d4, uint32_t d5d6d7, uint32_t d8d9d10);
---
+++ src/043-substring-divisibility/c/solve_test.c
@@ -0,0 +1,13 @@
+#include <test.h>
+#include "solve.h"
+
+void test_pandigital()
+{
+    assert(0 == pandigital(306, 357, 289));
+    assert(0 == pandigital(410, 293, 560));
+    assert(1406357289LL == pandigital(406, 357, 289));
+}
+
+TESTS() {
+    RUN(test_pandigital);
+}
