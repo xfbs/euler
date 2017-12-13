0f2277d8b1bb8d0c5d31819e341d178d94ad584f
Patrick Elsen <pelsen@xfbs.net>
2017-12-13 15:16:20 +0100

    [044/c] adds solution

---
+++ src/044-pentagon-numbers/c/solve.h
@@ -0,0 +1,9 @@
+#include <stdint.h>
+#include <stdbool.h>
+#pragma once
+
+uint64_t solve();
+uint64_t p(uint64_t x);
+uint64_t p_inverse(uint64_t _y);
+bool is_p(uint64_t x);
+uint64_t b(uint64_t n);
