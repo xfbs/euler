40f6437d694dbc96af6fcbf5813dcbef9c1ed184
Patrick Elsen <pelsen@xfbs.net>
2017-12-13 18:48:39 +0100

    fixes euler setup script

--- .setup_travis.sh
+++ .setup_travis.sh
@@ -4,7 +4,7 @@
 curl -sSf https://build.travis-ci.org/files/rustup-init.sh | sh -s -- --default-toolchain=beta -y
 
 # install crystal
-sudo apt-key adv --keyserver keys.gnupg.net --recv-keys 09617FD37CC06B54
-echo "deb https://dist.crystal-lang.org/apt crystal main" | sudo tee -a /etc/apt/sources.list.d/crystal.list
-sudo apt-get update
-sudo apt-get install crystal -y
+apt-key adv --keyserver keys.gnupg.net --recv-keys 09617FD37CC06B54
+echo "deb https://dist.crystal-lang.org/apt crystal main" | tee -a /etc/apt/sources.list.d/crystal.list
+apt-get update
+apt-get install crystal -y
