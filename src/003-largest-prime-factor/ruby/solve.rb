7bad095336f3995a6dfe0c8a0d9386ebc41b363b
Patrick Elsen <pelsen@xfbs.net>
2017-12-13 23:19:54 +0100

    [003/ruby] adds solution

---
+++ src/003-largest-prime-factor/ruby/solve.rb
@@ -0,0 +1,35 @@
+#!/usr/bin/env ruby
+
+module Solve
+  def self.solve(num)
+    cur = 2
+    while !is_prime?(num)
+      if (num % cur) == 0
+        num /= cur
+      else
+        cur += 1
+        while !is_prime?(cur)
+          cur += 1
+        end
+      end
+    end
+
+    num
+  end
+
+  def self.is_prime?(num)
+    return false if num < 2
+    return false if num > 2 && (num % 2) == 0
+
+    max_check = Math.sqrt(num)
+    (3..max_check).step(2) do |d|
+      return false if (num % d) == 0
+    end
+
+    true
+  end
+end
+
+if __FILE__ == $0
+  puts Solve.solve(600851475143)
+end
