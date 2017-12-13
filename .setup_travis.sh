f8c598098ffead21c41e634c5cfefe873cdb2034
Patrick Elsen <pelsen@xfbs.net>
2017-12-13 18:44:11 +0100

    experimental - adds setup script for travis

---
+++ .setup_travis.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# install rust beta
+curl -sSf https://build.travis-ci.org/files/rustup-init.sh | sh -s -- --default-toolchain=beta -y
+
+# install crystal
+sudo apt-key adv --keyserver keys.gnupg.net --recv-keys 09617FD37CC06B54
+echo "deb https://dist.crystal-lang.org/apt crystal main" | sudo tee -a /etc/apt/sources.list.d/crystal.list
+sudo apt-get update
+sudo apt-get install crystal -y
