f410614bff33b36e7cec8d158c3e1b9d9c27e678
Patrick Elsen <pelsen@xfbs.net>
2017-12-14 12:08:32 +0100

    [009/ruby] updates solution

--- src/009-special-pythagorean-triplet/ruby/solve.rb
+++ src/009-special-pythagorean-triplet/ruby/solve.rb
@@ -1,4 +1,21 @@
-#!/usr/bin/env ruby
-require_relative 'solution'
+module Solver
+  def self.solve n
+    (((n-3)/3)..(n-1)).each do |c|
+      (((n-c-1)/2)..[c, n-c].min).each do |b|
+        a = n - c - b
+        if is_pythagorean_triplet?(a, b, c)
+          return a*b*c
+        end
+      end
+    end
+  end
 
-puts Solution.solve(1000)
+  def self.is_pythagorean_triplet?(a, b, c)
+    return false if a > b
+    return false if b > c
+    return false unless a**2 + b**2 == c**2
+    true
+  end
+end
+
+puts Solver.solve(1000)
--- src/009-special-pythagorean-triplet/ruby/test.rb
+++ src/009-special-pythagorean-triplet/ruby/test.rb
@@ -1,20 +1,18 @@
 #!/usr/bin/env ruby
-require_relative 'solution'
-require 'minitest'
+require 'minitest/autorun'
+require_relative 'solve'
 
-class SolutionTest < MiniTest::Test
+class SolverTest < MiniTest::Test
   def setup
   end
 
   def test_solution
-    assert_equal 60, Solution.solve(12)
+    assert_equal 60, Solver.solve(12)
   end
 
   def test_is_pythagorean_triplet?
-    assert Solution.is_pythagorean_triplet?(3, 4, 5)
-    assert !Solution.is_pythagorean_triplet?(4, 3, 5)
-    assert !Solution.is_pythagorean_triplet?(3, 4, 6)
+    assert Solver.is_pythagorean_triplet?(3, 4, 5)
+    assert !Solver.is_pythagorean_triplet?(4, 3, 5)
+    assert !Solver.is_pythagorean_triplet?(3, 4, 6)
   end
 end
-
-MiniTest.autorun
