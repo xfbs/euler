030da0efe5105d3f74854f49e80f90dc6284ed79
Patrick Elsen <pelsen@xfbs.net>
2017-12-14 11:58:00 +0100

    [009/crystal] adds solution

---
+++ src/009-special-pythagorean-triplet/crystal/test/solver_test.cr
@@ -0,0 +1,14 @@
+require "minitest/autorun"
+require "../src/solver"
+
+class SolverTest < Minitest::Test
+  def test_solution
+    assert_equal 60, Solver.solve(12)
+  end
+
+  def test_is_pythagorean_triplet?
+    assert Solver.is_pythagorean_triplet?(3, 4, 5)
+    assert !Solver.is_pythagorean_triplet?(4, 3, 5)
+    assert !Solver.is_pythagorean_triplet?(3, 4, 6)
+  end
+end
