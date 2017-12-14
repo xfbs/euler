5279ecdb53f1394b1f8771b3ebf990f99137b11c
Patrick Elsen <pelsen@xfbs.net>
2017-12-14 13:40:07 +0100

    [013/crystal] adds solution

---
+++ src/013-large-sum/crystal/src/solve.cr
@@ -0,0 +1,5 @@
+require "./solver"
+
+raise "no data file supplied" unless ARGV[0]
+numbers = File.read(ARGV[0]).split("\n").select{|l| !l.empty?}
+puts Solver.solve(numbers)
---
+++ src/013-large-sum/crystal/src/solver.cr
@@ -0,0 +1,5 @@
+module Solver
+  def self.solve(nums)
+    nums.map{|n| n[0..10].to_u64}.sum.to_s[0..9].to_u64
+  end
+end
