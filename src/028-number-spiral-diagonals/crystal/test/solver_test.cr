c5432a4ad79f24e48dd84ff5b15d81166194cf54
Patrick Elsen <pelsen@xfbs.net>
2017-12-15 08:04:49 +0100

    [028/crystal] adds solution

---
+++ src/028-number-spiral-diagonals/crystal/test/solver_test.cr
@@ -0,0 +1,12 @@
+require "minitest/autorun"
+require "../src/solver"
+
+class SolverTest < Minitest::Test
+  def setup
+  end
+
+  def test_solve
+    assert_equal Solver.solve(3), 25
+    assert_equal Solver.solve(5),101
+  end
+end
