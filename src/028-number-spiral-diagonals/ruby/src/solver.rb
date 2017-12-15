01ed048c58962bdeef9f7e8f2385b4bec06f8d4f
Patrick Elsen <pelsen@xfbs.net>
2017-12-15 08:03:14 +0100

    [028/ruby] adds solution

---
+++ src/028-number-spiral-diagonals/ruby/src/solver.rb
@@ -0,0 +1,15 @@
+module Solver
+  def self.solve(max)
+    sum = 1
+    cur = 1
+
+    (2...max).step(2).each do |ring|
+      cur += ring; sum += cur;
+      cur += ring; sum += cur;
+      cur += ring; sum += cur;
+      cur += ring; sum += cur;
+    end
+
+    sum
+  end
+end
---
+++ src/028-number-spiral-diagonals/ruby/test/solver_test.rb
@@ -0,0 +1,12 @@
+require 'minitest/autorun'
+require_relative '../src/solver'
+
+class SolverTest < MiniTest::Test
+  def setup
+  end
+
+  def test_solve
+    assert_equal Solver.solve(3), 25
+    assert_equal Solver.solve(5),101
+  end
+end
