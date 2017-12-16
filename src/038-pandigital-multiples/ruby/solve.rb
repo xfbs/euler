5f1f3dbda966943798ca82df48dca34b4c01e9ca
Patrick Elsen <pelsen@xfbs.net>
2017-12-16 13:26:32 +0100

    [038/ruby] optimises solution a little bit

--- src/038-pandigital-multiples/ruby/solve.rb
+++ src/038-pandigital-multiples/ruby/solve.rb
@@ -1,13 +1,12 @@
-#!/usr/bin/env ruby
-
 module Solve
   def self.solve
-    pandigital = []
+    pandigitals = []
     (1..9999).each do |b|
-      pandigital << check_pandigital_multiples(b)
+      pandigital = check_pandigital_multiples(b)
+      pandigitals << pandigital if pandigital
     end
 
-    pandigital.compact.max
+    pandigitals.max
   end
 
   def self.check_pandigital_multiples base
@@ -16,18 +15,25 @@ module Solve
       return num if works
       break if num > 987654321
     end
-    
+
     nil
   end
 
   def self.check_pandigital_multiple base, n
-    num = (1..n).map{|m| (m*base).to_s}.join.to_i
+    num = (1..n).map{|n| (n*base).to_s}.join.to_i
 
     return check_pandigital(num), num
   end
 
   def self.check_pandigital num
-    num.to_s.chars.sort.join == "123456789"
+    chars = num.to_s.chars
+    return false if chars.size != 9
+
+    "123456789".chars.each do |c|
+      return false unless chars.count{|i| i == c} == 1
+    end
+
+    true
   end
 end
 
