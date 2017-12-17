7712885c8ebf024e8c62ab83a6cd9644981d66b6
Patrick Elsen <pelsen@xfbs.net>
2017-12-17 13:32:52 +0100

    [004/rust] updates solution

--- src/004-largest-palindrome-product/rust/src/lib.rs
+++ src/004-largest-palindrome-product/rust/src/lib.rs
@@ -1,23 +1,5 @@
-trait Palindrome : PartialEq + Clone {
-    fn reversed(&self) -> Self;
-    fn is_palindrome(&self) -> bool {
-        *self == self.reversed()
-    }
-}
-
-impl Palindrome for i32 {
-    fn reversed(&self) -> i32 {
-        let mut r = 0;
-        let mut o = self.clone();
-
-        while o != 0 {
-            r = r * 10 + (o % 10);
-            o = o / 10;
-        }
-
-        r
-    }
-}
+mod palindrome;
+use palindrome::Palindrome;
 
 pub fn solve() -> i32 {
     (100..1000).map(|i|
---
+++ src/004-largest-palindrome-product/rust/src/palindrome.rs
@@ -0,0 +1,36 @@
+pub trait Palindrome : PartialEq + Clone {
+    fn reversed(&self) -> Self;
+    fn is_palindrome(&self) -> bool {
+        *self == self.reversed()
+    }
+}
+
+impl Palindrome for i32 {
+    fn reversed(&self) -> i32 {
+        let mut r = 0;
+        let mut o = self.clone();
+
+        while o != 0 {
+            r = r * 10 + (o % 10);
+            o = o / 10;
+        }
+
+        r
+    }
+}
+
+#[test]
+fn test_palindromes() {
+    assert!(121.is_palindrome());
+    assert!(32123.is_palindrome());
+    assert!(11511.is_palindrome());
+    assert!(923329.is_palindrome());
+    assert!(23232.is_palindrome());
+    assert!(55055.is_palindrome());
+
+    assert!(!123.is_palindrome());
+    assert!(!9989.is_palindrome());
+    assert!(!123432.is_palindrome());
+    assert!(!110.is_palindrome());
+    assert!(!5243.is_palindrome());
+}
