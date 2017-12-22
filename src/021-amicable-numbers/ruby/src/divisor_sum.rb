02c08c23f6798f14f3f7accaa7e1e39510f1cfbb
Patrick Elsen <pelsen@xfbs.net>
2017-12-22 23:12:08 +0100

    [021/ruby] exports divisor_sum method

---
+++ src/021-amicable-numbers/ruby/src/divisor_sum.rb
@@ -0,0 +1,15 @@
+module Euler
+  def self.divisor_sum(num)
+    (1..Math.sqrt(num)).map do |d|
+      if (num % d) == 0
+        if (d != (num / d)) && d != 1
+          d + (num / d)
+        else
+          d
+        end
+      else
+        0
+      end
+    end.inject(0, :+)
+  end
+end
--- src/021-amicable-numbers/ruby/src/solver.rb
+++ src/021-amicable-numbers/ruby/src/solver.rb
@@ -1,22 +1,10 @@
+require_relative "divisor_sum"
+
 module Solver
-  def self.divisor_sum(num)
-    (1..Math.sqrt(num)).map do |d|
-      if (num % d) == 0
-        if (d != (num / d)) && d != 1
-          d + (num / d)
-        else
-          d
-        end
-      else
-        0
-      end
-    end.inject(0, :+)
-  end
-
   def self.amicable_pair(num)
-    partner = divisor_sum(num)
+    partner = Euler.divisor_sum(num)
 
-    if num != partner && num == divisor_sum(partner)
+    if num != partner && num == Euler.divisor_sum(partner)
       partner
     else
       nil
