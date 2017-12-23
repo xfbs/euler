550412d114b3e9bd882425c693c766a88abbcc7e
Patrick Elsen <pelsen@xfbs.net>
2017-12-23 17:14:37 +0100

    [030/ruby] fixes broken test

--- src/030-digit-fifth-powers/ruby/test/solver_test.rb
+++ src/030-digit-fifth-powers/ruby/test/solver_test.rb
@@ -2,11 +2,4 @@ require 'minitest/autorun'
 require_relative '../src/solver'
 
 class SolverTest < MiniTest::Test
-  def setup
-  end
-
-  def test_solver
-    assert_equal Solver.solve(2), 6
-    assert_equal Solver.solve(4), 28
-  end
 end
