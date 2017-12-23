ae7b61e68387d6ccf9bfa7c8fce6d1cc64329cdf
Patrick Elsen <pelsen@xfbs.net>
2017-12-23 17:50:39 +0100

    [017/ruby] simplifies solution

--- src/017-number-letter-counts/ruby/src/solver.rb
+++ src/017-number-letter-counts/ruby/src/solver.rb
@@ -1,63 +1,74 @@
 module Solver
   def self.solve n
-    (1..n).map{|num| thousands_number_word(num).scan(/[a-z]/).length}.inject(:+)
+    (1..n).map{|num| thousands_number_word(num)}.inject(:+)
   end
 
   def self.teens_number_word n
     numbers = {
-      1 => 'one',
-      2 => 'two',
-      3 => 'three',
-      4 => 'four',
-      5 => 'five',
-      6 => 'six',
-      7 => 'seven',
-      8 => 'eight',
-      9 => 'nine',
-      10 => 'ten',
-      11 => 'eleven',
-      12 => 'twelve',
-      13 => 'thirteen',
-      14 => 'fourteen',
-      15 => 'fifteen',
-      16 => 'sixteen',
-      17 => 'seventeen',
-      18 => 'eighteen',
-      19 => 'nineteen',
-      20 => 'twenty',
-      30 => 'thirty',
-      40 => 'forty',
-      50 => 'fifty',
-      60 => 'sixty',
-      70 => 'seventy',
-      80 => 'eighty',
-      90 => 'ninety'
+      1  => 3, # one
+      2  => 3, # two
+      3  => 5, # three
+      4  => 4, # four
+      5  => 4, # five
+      6  => 3, # six
+      7  => 5, # seven
+      8  => 5, # eight
+      9  => 4, # nine
+      10 => 3, # ten
+      11 => 6, # eleven
+      12 => 6, # twelve
+      13 => 8, # thirteen
+      14 => 8, # fourteen
+      15 => 7, # fifteen
+      16 => 7, # sixteen
+      17 => 9, # seventeen
+      18 => 8, # eighteen
+      19 => 8, # nineteen
+      20 => 6, # twenty
+      30 => 6, # thirty
+      40 => 5, # forty
+      50 => 5, # fifty
+      60 => 5, # sixty
+      70 => 7, # seventy
+      80 => 6, # eighty
+      90 => 6, # ninety
     }
 
-    r = numbers.sort_by{|p| p[0]}.reverse.inject([n, []]) do |s, cur|
+    numbers.sort_by{|p| p[0]}.reverse.inject([n, 0]) do |s, cur|
       if s[0] >= cur[0]
-        [s[0]-cur[0], s[1]+[cur[1]]]
+        [s[0]-cur[0], s[1]+cur[1]]
       else
         s
       end
-    end
-
-    r[1].join('-')
+    end[1]
   end
 
   def self.hundreds_number_word n
-    out = []
+    out = 0
     hundreds = n/100
-    out << "#{teens_number_word hundreds} hundred" if hundreds > 0
-    out << teens_number_word(n%100) if (n%100) > 0
-    out.join(' and ')
+
+    # thirty-three hundred
+    out += teens_number_word(hundreds) + 7 if hundreds > 0
+
+    # and
+    out += 3 if hundreds > 0 && (n % 100) > 0
+
+    # fourty-five
+    out += teens_number_word(n%100) if (n%100) > 0
+
+    out
   end
 
   def self.thousands_number_word n
-    out = []
+    out = 0
     thousands = n/1000
-    out << "#{hundreds_number_word thousands} thousand" if thousands > 0
-    out << hundreds_number_word(n%1000) if (n%1000) > 0
-    out.join(', ')
+
+    # three hundred and forty-five thousand
+    out += hundreds_number_word(thousands) + 8 if thousands > 0
+
+    # five hundred and sixty-seven
+    out += hundreds_number_word(n%1000) if (n%1000) > 0
+
+    out
   end
 end
--- src/017-number-letter-counts/ruby/test/solver_test.rb
+++ src/017-number-letter-counts/ruby/test/solver_test.rb
@@ -2,29 +2,41 @@ require 'minitest/autorun'
 require_relative '../src/solver'
 
 class SolverTest < MiniTest::Test
-  def setup
-  end
-
-  def test_solution
-  end
-
   def test_teens_number_word
-    assert_equal "one", Solver.teens_number_word(1)
-    assert_equal "five", Solver.teens_number_word(5)
-    assert_equal "seven", Solver.teens_number_word(7)
-    assert_equal "thirteen", Solver.teens_number_word(13)
-    assert_equal "twenty", Solver.teens_number_word(20)
-    assert_equal "twenty-one", Solver.teens_number_word(21)
-    assert_equal "forty-five", Solver.teens_number_word(45)
-    assert_equal "ninety-eight", Solver.teens_number_word(98)
-    assert_equal "seventy", Solver.teens_number_word(70)
-    assert_equal "thirty-three", Solver.teens_number_word(33)
+    assert_equal  3, Solver.teens_number_word(1)  # one
+    assert_equal  4, Solver.teens_number_word(5)  # four
+    assert_equal  5, Solver.teens_number_word(7)  # seven
+    assert_equal  8, Solver.teens_number_word(13) # thirteen
+    assert_equal  6, Solver.teens_number_word(20) # twenty
+    assert_equal  9, Solver.teens_number_word(21) # twenty-one
+    assert_equal  9, Solver.teens_number_word(45) # forty-five
+    assert_equal 11, Solver.teens_number_word(98) # ninety-eight
+    assert_equal  7, Solver.teens_number_word(70) # seventy
+    assert_equal 11, Solver.teens_number_word(33) # thirty-three
   end
 
   def test_hundreds_number_word
-    assert_equal "fifty-five", Solver.hundreds_number_word(55)
-    assert_equal "one hundred", Solver.hundreds_number_word(100)
-    assert_equal "one hundred and fifty-five", Solver.hundreds_number_word(155)
-    assert_equal "seven hundred and five", Solver.hundreds_number_word(705)
+    # fifty-five
+    assert_equal 9, Solver.hundreds_number_word(55)
+
+    # one-hundred
+    assert_equal 10, Solver.hundreds_number_word(100)
+
+    # one hundred and fifty-five
+    assert_equal 22, Solver.hundreds_number_word(155)
+
+    # seven hundred and five
+    assert_equal 19, Solver.hundreds_number_word(705)
+
+    # nine hundred and ninety-four
+    assert_equal 24, Solver.hundreds_number_word(994)
+  end
+
+  def test_thousands_number_word
+    # one thousand
+    assert_equal 11, Solver.thousands_number_word(1000)
+
+    # one thousand, seven hundred and sixty-four
+    assert_equal 35, Solver.thousands_number_word(1764)
   end
 end
