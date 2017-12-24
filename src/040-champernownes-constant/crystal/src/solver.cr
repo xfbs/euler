855e0f13899f79f64ab334118ea758ed448a1d2a
Patrick Elsen <pelsen@xfbs.net>
2017-12-24 20:17:06 +0100

    [040/crystal] adds solution

---
+++ src/040-champernownes-constant/crystal/src/solver.cr
@@ -0,0 +1,27 @@
+module Solver
+  # generates the product of the first, tenth, hundredth, etc. up to millionth
+  # digit of the champernowne constant
+  def self.solve
+    [0, 9, 99, 999, 9999, 99999, 999999].map do |n|
+      champernowne_digit(n)
+    end.reduce(1){|m, c| m * c}
+  end
+
+  # generates the digit that is at pos after the decimal point in the
+  # champernowne constant (zero being the first digit after the decimal)
+  def self.champernowne_digit(pos)
+    mag = 1
+    exp = 9
+
+    while pos >= (mag*exp)
+        pos -= mag*exp
+        exp *= 10
+        mag += 1
+    end
+
+    off = mag - pos % mag - 1
+    num = ((exp/9) + pos / mag) / 10**off
+
+    num % 10
+  end
+end
---
+++ src/040-champernownes-constant/crystal/test/solver_test.cr
@@ -0,0 +1,13 @@
+require "minitest/autorun"
+require "../src/solver"
+
+class SolverTest < Minitest::Test
+  def test_champernowne_digit
+    (1..1000)
+      .map{|n| n.to_s.split("").map{|n| n.to_i}}
+      .flatten
+      .each_with_index do |d, i|
+      assert_equal d, Solver.champernowne_digit(i)
+    end
+  end
+end
