6f98f5aa8ca5866470ac66c292dc353e78b6a34d
Patrick Elsen <pelsen@xfbs.net>
2017-12-24 19:52:55 +0100

    [052/c] adds solution

---
+++ src/052-permuted-multiples/c/solve.h
@@ -0,0 +1,21 @@
+#include <stdint.h>
+#include <stdbool.h>
+#pragma once
+
+typedef struct {
+    // how many digits are there in this number? eg. 0 if the number is 0, 1 if
+    // the number is 1..9, 2 if the the number is 10..99
+    uint8_t count;
+
+    // the digits of the number, in reverse order (lowest magnitude first)
+    uint8_t digits[20];
+} digits_t;
+
+// find a number x such that 2x, 3x, 4x, 5x, and 6x contain the same digits.
+uint64_t solve();
+
+// for a given number x, check if 2x, 3x, 4x, 5x and 6x contain the same digits
+bool is_permuted_multiple(uint64_t x);
+
+// take a number and compute its digits in the form of a digits_t struct.
+digits_t num_to_digits(uint64_t n);
