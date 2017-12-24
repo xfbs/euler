72e95512d3f95a36ee30d2443271b28b1aec7b04
Patrick Elsen <pelsen@xfbs.net>
2017-12-24 20:14:34 +0100

    [040/ruby] adds solution

---
+++ src/040-champernownes-constant/ruby/test/solver_test.rb
@@ -0,0 +1,13 @@
+require 'minitest/autorun'
+require_relative '../src/solver'
+
+class SolverTest < MiniTest::Test
+  def test_champernowne_digit
+    (1..1000)
+      .map{|n| n.to_s.split("").map(&:to_i)}
+      .flatten
+      .each_with_index do |d, i|
+      assert_equal d, Solver.champernowne_digit(i)
+    end
+  end
+end
