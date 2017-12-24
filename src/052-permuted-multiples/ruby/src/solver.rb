10797e7cf35b50c5b05f9fe67c8b1453986a5d74
Patrick Elsen <pelsen@xfbs.net>
2017-12-24 18:47:39 +0100

    [052/ruby] adds comments and tests

--- src/052-permuted-multiples/ruby/src/solver.rb
+++ src/052-permuted-multiples/ruby/src/solver.rb
@@ -1,9 +1,7 @@
 module Solver
+  # finds the smallest integer x, such that 2x, 3x, 4x, 5x, and 6x contain the
+  # same digits.
   def self.solve
-    # almost a one-liner and cute, but not exactly efficient.
-    #(1..100000000)
-    #  .find{|n| [2,3,4,5,6].map{|i| (i*n).to_s.chars.sort}.uniq.length == 1}
-
     (2..10).each do |exp|
       ((10**exp)..(10**(exp+1) / 6)).each do |i|
         return i if is_permuted_multiple?(i)
@@ -11,16 +9,18 @@ module Solver
     end
   end
 
-  def self.is_permuted_multiple? i
-    digits = num_to_digits_rev(2*i).sort
+  # for a given number x, check if 2x, 3x, 4x, 5x and 6x contain the same digits
+  def self.is_permuted_multiple? x
+    digits = num_to_digits_rev(2*x).sort
 
     [3,4,5,6].each do |n|
-      return false unless digits == num_to_digits_rev(n*i).sort
+      return false unless digits == num_to_digits_rev(n*x).sort
     end
 
     true
   end
 
+  # takes a number n and returns an array with the digits (in reverse order)
   def self.num_to_digits_rev n
     digits = []
 
