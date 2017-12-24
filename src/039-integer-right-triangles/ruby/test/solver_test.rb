f0095c1b2d4e6af1e93f9372615f4e678c239dd5
Patrick Elsen <pelsen@xfbs.net>
2017-12-24 03:05:13 +0100

    [039/ruby] simplifies test

--- src/039-integer-right-triangles/ruby/test/solver_test.rb
+++ src/039-integer-right-triangles/ruby/test/solver_test.rb
@@ -2,9 +2,6 @@ require 'minitest/autorun'
 require_relative '../src/solver'
 
 class SolverTest < MiniTest::Test
-  def setup
-  end
-
   def test_number_of_solutions
     assert_equal 3, Solver.number_of_solutions(120)
   end
