483dd5bb8e947b81dc806c7fead5186bc1f91e5f
Patrick Elsen <pelsen@xfbs.net>
2017-12-24 02:44:18 +0100

    [042/ruby] updates solution

---
+++ src/042-coded-triangle-numbers/ruby/test/solver_test.rb
@@ -0,0 +1,10 @@
+require 'minitest/autorun'
+require_relative '../src/solver'
+
+class SolverTest < MiniTest::Test
+  def test_solution
+    assert_equal Solver.solve(["SKY"]), 1
+    assert_equal Solver.solve(["SKY", "HAMBURGER"]), 1
+    assert_equal Solver.solve(["HAMBURGER"]), 0
+  end
+end
