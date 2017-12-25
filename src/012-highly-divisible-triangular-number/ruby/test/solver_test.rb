2a56d31d728d60408ae1e2358af1a3d9e5836300
Patrick Elsen <pelsen@xfbs.net>
2017-12-25 02:14:16 +0100

    [012/ruby] adds test

--- src/012-highly-divisible-triangular-number/ruby/test/solver_test.rb
+++ src/012-highly-divisible-triangular-number/ruby/test/solver_test.rb
@@ -2,11 +2,26 @@ require 'minitest/autorun'
 require_relative '../src/solver'
 
 class SolverTest < MiniTest::Test
-  def setup
-  end
-
   def test_solver
     assert_equal Solver.solve(2), 6
     assert_equal Solver.solve(4), 28
+  end
+
+  def test_factor_count
+    p = Prime.new
+    assert_equal Solver.factor_count(p, 2), 2
+    assert_equal Solver.factor_count(p, 4), 3
+    assert_equal Solver.factor_count(p, 8), 4
+    assert_equal Solver.factor_count(p, 16), 5
+    assert_equal Solver.factor_count(p, 64), 7
+    assert_equal Solver.factor_count(p, 32), 6
+
+    assert_equal Solver.factor_count(p, 1), 1
+    assert_equal Solver.factor_count(p, 3), 2
+    assert_equal Solver.factor_count(p, 6), 4
+    assert_equal Solver.factor_count(p, 10), 4
+    assert_equal Solver.factor_count(p, 15), 4
+    assert_equal Solver.factor_count(p, 21), 4
+    assert_equal Solver.factor_count(p, 28), 6
   end
 end
