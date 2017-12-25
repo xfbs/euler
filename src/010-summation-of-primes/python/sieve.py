79359f7559ac7f5296152b0e19918c10f289a239
Patrick Elsen <pelsen@xfbs.net>
2017-12-25 01:08:59 +0100

    [010/python] improves speed, adds tests

--- src/010-summation-of-primes/python/sieve.py
+++ src/010-summation-of-primes/python/sieve.py
@@ -1,18 +1,28 @@
 def iter(m):
-   nums = [True for _ in range(m)]
+   # generate sieve, half the size of m because we know all odd numbers apart
+   # from 2 are not prime.
+   nums = [True for _ in range((m + 1) // 2)]
+
+   # 1 is not prime
    nums[0] = False
-   nums[1] = False
 
-   p = 2
-   while p**2 < m:
-      for n in range(p**2, m, p):
-         nums[n] = False
+   p = 3
+   p2 = p**2
 
-      p += 1
-      while not nums[p]:
-         p += 1
+   # for every prime such that p^2 < m
+   while p2 < m:
+      # check off all multiples
+      for n in range(p**2, m, 2*p):
+         nums[(n - 1) // 2] = False
 
+      p += 2
+      while not nums[(p - 1) // 2]:
+         p += 2
+
+      p2 = p**2
+
+   yield 2
    for num, prim in enumerate(nums):
       if prim:
-         yield num
+         yield 2 * num + 1
 
--- src/010-summation-of-primes/python/test/test_sieve.py
+++ src/010-summation-of-primes/python/test/test_sieve.py
@@ -9,6 +9,13 @@ class TestSolution(unittest.TestCase):
       self.assertEqual(next(i), 5)
       self.assertEqual(next(i), 7)
 
+      i = sieve.iter(11)
+      self.assertEqual(next(i), 2)
+      self.assertEqual(next(i), 3)
+      self.assertEqual(next(i), 5)
+      self.assertEqual(next(i), 7)
+      self.assertEqual(next(i), 11)
+
       i = sieve.iter(20)
       self.assertEqual(next(i), 2)
       self.assertEqual(next(i), 3)
@@ -18,3 +25,8 @@ class TestSolution(unittest.TestCase):
       self.assertEqual(next(i), 13)
       self.assertEqual(next(i), 17)
       self.assertEqual(next(i), 19)
+
+   def test_iter_max(self):
+      for max in [13, 34, 55, 123]:
+         for p in sieve.iter(max):
+            self.assertTrue(p <= max)
