3dfb34a51641d50efd1ee53c6134cbe9e5754847
Patrick Elsen <pelsen@xfbs.net>
2017-12-25 14:24:09 +0100

    [003/python] improves prime module

--- src/003-largest-prime-factor/python/prime.py
+++ src/003-largest-prime-factor/python/prime.py
@@ -1,30 +1,40 @@
 import math
 
-def check(primes, prime):
-   root = math.sqrt(prime)
-   for d in primes:
-      if d > root:
-         return True
-      if (prime % d) == 0:
-         return False
+class Prime(object):
+   def __init__(self):
+      self.primes = [2]
+      self.prime  = 2
 
-   return True
+   def check(self, prime):
+      root = math.sqrt(prime)
+      for d in self.primes:
+         if d > root:
+            return True
+         if (prime % d) == 0:
+            return False
+      return True
+
+   def next(self):
+      if self.prime == 2:
+         self.prime = 3
+         return 2
+      self.primes.append(self.prime)
+
+      # find next prime
+      self.prime += 2
+      while not self.check(self.prime):
+         self.prime += 2
+
+      return self.primes[-1]
+
+   def nth(self, n):
+      while len(self.primes) < n:
+         self.next()
+      return self.primes[n-1]
 
 # generator function for primes
 def iter():
-   # the only even prime number
-   yield 2
-
-   primes = [2]
-   prime = 3
-
+   p = Prime()
    while True:
-      # yield current prime number
-      yield prime
-      primes.append(prime)
-
-      # find next prime
-      prime += 2
-      while not check(primes, prime):
-         prime += 2
+      yield p.next()
 
