5db3d176360fa4eea3d758999e78a88063d6032f
Patrick Elsen <pelsen@xfbs.net>
2017-12-25 17:43:57 +0100

    [004/python] fixes tests

--- src/004-largest-palindrome-product/python/test/test_solver.py
+++ src/004-largest-palindrome-product/python/test/test_solver.py
@@ -3,10 +3,5 @@ import solver
 
 class TestSolution(unittest.TestCase):
    def test_solve(self):
-      self.assertEqual(solver.solve(10), 5)
-      self.assertEqual(solver.solve(15), 5)
-      self.assertEqual(solver.solve(20), 5)
-      self.assertEqual(solver.solve(9), 3)
-      self.assertEqual(solver.solve(91), 13)
-      self.assertEqual(solver.solve(323), 19)
+      self.assertEqual(solver.solve(2), 9009)
 
