2f29b17740ccedd679ba93923b01810908b4ac24
Patrick Elsen <pelsen@xfbs.net>
2017-12-26 14:05:45 +0100

    updates table generation code

--- .table.rb
+++ .table.rb
@@ -4,9 +4,19 @@ lang = data.map{|p, i| i.map{|l, t| l}}.flatten.uniq
 lang = lang.sort_by{|l| data.values.map{|i| i.keys}.flatten.count{|i| i == l}}
 lang = lang.reverse
 
-puts "| problem | #{lang.map{|l| l.rjust(3, ' ')}.join(' | ')} |"
-puts "| ------- | #{lang.map{|l| l.rjust(3, ' ').gsub(/./, '-')}.join(' | ')} |"
+puts "| problem | #{lang.map{|l| l.rjust(3, ' ')}.join(' | ')} | *avg* |"
+puts "| ------- | #{lang.map{|l| l.rjust(3, ' ').gsub(/./, '-')}.join(' | ')} | ----- |"
 
 data.keys.each do |key|
-  puts "| `#{key}` | #{lang.map{|l| (data[key][l] || "").to_s.rjust(4, ' ')}.join(' | ')} |"
+  avg = data[key].values.inject(0, :+) / data[key].values.length
+  puts "| `#{key}` | #{lang.map{|l| (data[key][l] || "").to_s.rjust(4, ' ')}.join(' | ')} | #{avg} |"
 end
+
+counts = lang.map{|l| data.values.map{|i| i.keys}.flatten.count{|i| i == l}}
+puts "| *count* | #{counts.join(' | ')} | #{counts.inject(0, :+) / counts.length} |"
+
+avgs = lang.map do |l|
+  res = data.values.map{|i| i[l]}.compact
+  res.inject(0, :+) / res.length
+end
+puts "| *average* | #{avgs.join(' | ')} | #{avgs.inject(0, :+) / avgs.length} |"
