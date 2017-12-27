ba3d798882b42ddde20703d06b423f516b7d985c
Patrick Elsen <pelsen@xfbs.net>
2017-12-27 01:26:17 +0100

    [014/python] adds tests

--- src/014-longest-collatz-sequence/python/test/test_solver.py
+++ src/014-longest-collatz-sequence/python/test/test_solver.py
@@ -2,6 +2,15 @@ import unittest
 import solver
 
 class TestSolution(unittest.TestCase):
-    def test_solve(self):
-       self.assertEqual(solver.solve(["123", "234"]), 357)
-       self.assertEqual(solver.solve(["213563345435254532"]), 2135633454)
+   def test_solve(self):
+      self.assertEqual(solver.solve(20), 19)
+
+   def test_collatz(self):
+      c = solver.CollatzLength()
+      self.assertEqual(c.len(1), 1)
+      self.assertEqual(c.len(2), 2)
+      self.assertEqual(c.len(5), 6)
+      self.assertEqual(c.len(11), 15)
+      self.assertEqual(c.len(13), 10)
+      self.assertEqual(c.len(19), 21)
+      self.assertEqual(c.len(32), 6)
