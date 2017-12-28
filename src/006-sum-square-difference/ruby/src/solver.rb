e1f9dcbf65b704d351528e6792997ef3f10dfad5
Patrick Elsen <pelsen@xfbs.net>
2017-12-28 14:46:03 +0100

    [006/ruby] updates solution

--- src/006-sum-square-difference/ruby/src/solver.rb
+++ src/006-sum-square-difference/ruby/src/solver.rb
@@ -3,8 +3,3 @@ module Solver
     (3 * n**4 + 2 * n**3 - 3 * n**2 - 2 * n) / 12
   end
 end
-
-if __FILE__ == $0
-  puts Solver.solve(100)
-end
-
--- src/006-sum-square-difference/ruby/test/solver_test.rb
+++ src/006-sum-square-difference/ruby/test/solver_test.rb
@@ -1,11 +1,7 @@
-#!/usr/bin/env ruby
 require 'minitest/autorun'
-require_relative 'solve'
+require_relative '../src/solver'
 
 class SolverTest < MiniTest::Test
-  def setup
-  end
-
   def test_solution
     assert_equal 2640, Solver.solve(10)
   end
