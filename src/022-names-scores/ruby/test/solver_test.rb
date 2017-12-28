588cd78503992759991142519588de437310813c
Patrick Elsen <pelsen@xfbs.net>
2017-12-28 20:25:02 +0100

    [022/ruby] fixes tests

--- src/022-names-scores/ruby/test/solver_test.rb
+++ src/022-names-scores/ruby/test/solver_test.rb
@@ -3,8 +3,8 @@ require_relative '../src/solver'
 
 class SolverTest < MiniTest::Test
   def test_weight
-    assert_equal 1, Solution.weight("A")
-    assert_equal 26, Solution.weight("Z")
-    assert_equal 53, Solution.weight("COLIN")
+    assert_equal 1, Solver.weight("A")
+    assert_equal 26, Solver.weight("Z")
+    assert_equal 53, Solver.weight("COLIN")
   end
 end
