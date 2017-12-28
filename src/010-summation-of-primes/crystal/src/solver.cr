9f477d3a942cecdfec4c19ffd18d2d5b5735a241
Patrick Elsen <pelsen@xfbs.net>
2017-12-28 02:49:52 +0100

    [010/crystal] simplifies solution

--- src/010-summation-of-primes/crystal/src/solver.cr
+++ src/010-summation-of-primes/crystal/src/solver.cr
@@ -2,15 +2,9 @@ require "euler/prime"
 
 module Solver
   def self.solve(max)
-    sum = 0_u64
-    pos = 0
-    p = Euler::Prime.new
-
-    while p.nth(pos) < max
-      sum += p.nth(pos)
-      pos += 1
-    end
-
-    sum
+    Euler::Prime.new
+      .iter
+      .take_while{|n| n < max}
+      .sum
   end
 end
