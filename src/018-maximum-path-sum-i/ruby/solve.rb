917441f3ae94531f841c6b3d9a93dee986394c2a
Patrick Elsen <pelsen@xfbs.net>
2017-12-28 20:07:26 +0100

    [018/ruby] removes old files

--- src/018-maximum-path-sum-i/ruby/solve.rb
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env ruby
-require_relative 'solution'
-
-throw "no datafile specified" if ARGV.length < 1
-triangle = File.open(ARGV[0]).read.split("\n").map{|l| l.split(" ").map{|e| e.to_i}}
-puts Solution.solve(triangle)
