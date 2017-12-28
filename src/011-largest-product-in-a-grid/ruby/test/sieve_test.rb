bd273c7bc62c12d99bfd3c2dbe9b67507e8583ef
Patrick Elsen <pelsen@xfbs.net>
2017-12-28 19:21:01 +0100

    [011/ruby] removes useless files

--- src/011-largest-product-in-a-grid/ruby/test/sieve_test.rb
+++
@@ -1,9 +0,0 @@
-require 'minitest/autorun'
-require_relative '../src/euler/sieve'
-
-class SieveTest < MiniTest::Test
-  def test_sieve
-    s = Euler::Sieve.new(100)
-    assert_equal s.to_a, [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97]
-  end
-end
