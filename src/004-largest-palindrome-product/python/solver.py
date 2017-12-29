b234ed7c399252a1c439adffbe131a62f28fa208
Patrick Elsen <pelsen@xfbs.net>
2017-12-29 19:42:18 +0100

    [004/python] improves solution speed

--- src/004-largest-palindrome-product/python/solver.py
+++ src/004-largest-palindrome-product/python/solver.py
@@ -1,28 +1,33 @@
+import math
+
 def palindrome(num):
    rev = 0
    cur = num
+   exp = 1
 
    while cur != 0:
       rev *= 10
       rev += cur % 10
       cur //= 10
+      exp *= 10
 
-   return rev == num
+   return ((num * exp) + rev, (num // 10 * exp) + rev)
+
+def factorizable(num, min_factor, max_factor):
+   for d in range(min_factor, math.floor(math.sqrt(num)) + 1):
+      if num % d == 0 and (num // d) >= min_factor and (num // d) < max_factor:
+         return True
+   return False
 
 def solve(m):
    # minimum number factor to consider
-   s = 10**(m-1)
-   e = s * 10
-   largest = 0
+   min_factor = 10**(m-1)
+   max_factor = min_factor * 10
 
-   # for all factors s <= a < e
-   for a in range(s, e):
-      # for all factors a <= b < e (larger than or equal to a to avoid
-      # duplications)
-      for b in range(a, e):
-         product = a * b
-         if palindrome(product):
-            if product > largest:
-               largest = product
+   for (palin, _) in map(palindrome, range(max_factor-1, min_factor-1, -1)):
+      if factorizable(palin, min_factor, max_factor):
+         return palin
 
-   return largest
+   for (_, palin) in map(palindrome, range(max_factor-1, min_factor-1, -1)):
+      if factorizable(palin, min_factor, max_factor):
+         return palin
--- src/004-largest-palindrome-product/python/test/test_solver.py
+++ src/004-largest-palindrome-product/python/test/test_solver.py
@@ -5,3 +5,13 @@ class TestSolution(unittest.TestCase):
    def test_solve(self):
       self.assertEqual(solver.solve(2), 9009)
 
+   def test_palindrome(self):
+      self.assertEqual(solver.palindrome(86), (8668, 868))
+      self.assertEqual(solver.palindrome(456), (456654, 45654))
+      self.assertEqual(solver.palindrome(9123), (91233219, 9123219))
+
+   def test_factorizable(self):
+      self.assertEqual(solver.factorizable(234 * 345, 234, 345), True)
+      self.assertEqual(solver.factorizable(234 * 345, 23, 34), False)
+      self.assertEqual(solver.factorizable(999999, 100, 999), False)
+
