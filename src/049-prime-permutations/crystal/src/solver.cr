af72dd7e62716beb61cc0dda1e0692903614c2ba
Patrick Elsen <pelsen@xfbs.net>
2017-12-30 09:30:11 +0100

    [049/crystal] adds solution

---
+++ src/049-prime-permutations/crystal/src/solver.cr
@@ -0,0 +1,40 @@
+require "euler/prime"
+
+module Solver
+  # find the prime smaller than max that is the sum of the most consecutive
+  # primes.
+  def self.solve(digits, seen)
+    primes = Euler::Prime.new
+    min = 10**(digits-1)
+    max = 10**digits
+
+    # generate all n-digit primes
+    candidates = primes.iter
+      .take_while{|p| p < max}
+      .select{|p| p != seen && p > min}
+      .to_a
+
+    candidates.each_with_index do |p, i|
+      candidates.each
+        .skip(i+1)
+        .take_while{|s| (p + 2 * (s-p)) < max}
+        .select{|s| test_sequence(primes, p, s-p)}
+        .select{|s| is_permutations(p, s-p)}
+        .each{|s| return (0..2).reduce(0u64){|m, c| m * 10**digits + p + c * (s-p)}}
+    end
+  end
+
+  def self.test_sequence(primes, p, s)
+    (0..2).each do |i|
+      return false unless primes.index(p + i*s)
+    end
+
+    true
+  end
+
+  def self.is_permutations(p, s)
+    list = [p, p+s, p+s+s]
+    list = list.map{|n| n.to_s.chars.map{|c| c.to_i}.sort}
+    list[0] == list[1] && list[1] == list[2]
+  end
+end
---
+++ src/049-prime-permutations/crystal/test/solver_test.cr
@@ -0,0 +1,22 @@
+require "minitest/autorun"
+require "../src/solver"
+
+class SolverTest < Minitest::Test
+  def test_solver
+    assert_equal Solver.solve(4, 0), 148748178147
+  end
+
+  def test_sequence
+    p = Euler::Prime.new
+    p.iter.take_while{|p| p < 10000}
+
+    assert Solver.test_sequence(p, 1487, 3330)
+    assert Solver.test_sequence(p, 1487, 3300)
+    assert !Solver.test_sequence(p, 1487, 3301)
+  end
+
+  def test_permutation
+    assert Solver.is_permutations(1487, 3330)
+    assert !Solver.is_permutations(1487, 1234)
+  end
+end
