775c3bba6708bc104cdbc3ed683c66e2650df3ad
Patrick Elsen <pelsen@xfbs.net>
2018-01-05 19:20:42 +0100

    [031/c] adds solution

---
+++ src/031-coin-sums/c/bin/main.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include "solve.h"
+
+int main(int argc, char *argv[])
+{
+    printf("%i\n", solve(200));
+
+    return 0;
+}
---
+++ src/031-coin-sums/c/src/solve.h
@@ -0,0 +1,6 @@
+#include <stdint.h>
+#include <stddef.h>
+#pragma once
+
+uint32_t solve(uint32_t value);
+uint32_t change(uint32_t coins[], uint32_t value, size_t pos);
