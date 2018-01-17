3b5bd943cf86689e2fefcb482d3d54e942d66cfd
Patrick Elsen <pelsen@xfbs.net>
2018-01-17 19:29:33 +0100

    [047/ruby] adds solution

---
+++ src/047-distinct-primes-factors/ruby/src/solver.rb
@@ -0,0 +1,65 @@
+require "euler/prime"
+
+module Solver
+  def self.solve(count)
+    max = 2200
+    primes = Euler::Prime.new
+
+    loop do
+      # create array of how many prime factors (up to count) each number has
+      prime_factors = Array.new(max, 0)
+      primes.take_while{|p| p < max}.each do |prime|
+        cur = prime
+
+        while cur < max
+          if prime_factors[cur] <= count
+            prime_factors[cur] += 1
+          end
+
+          cur += prime
+        end
+      end
+
+      # check which `count` consecutive numbers have `count` distinct prime
+      # factors
+      (0..prime_factors.size-count)
+        .select{|num| (0...count).all?{|c| prime_factors[num+c] == count}}
+        .select{|num| distinct_prime_factors(primes, count, num)}
+        .each{|num| return num}
+
+      max *= 2
+    end
+  end
+
+  def self.distinct_prime_factors(primes, count, num)
+    factors = (0...count).map{|o| num + o}.map do |cur|
+      res = []
+      state = 1
+      i = 0
+      while cur != state
+        factor = 1
+        prime = primes.nth(i)
+
+        while (cur % (factor * prime)) == 0
+          factor *= prime
+        end
+        state *= factor
+
+        if factor > 1
+          res << factor
+        end
+
+        i += 1
+      end
+
+      res
+    end.to_a
+
+    return false unless factors.all?{|f| f.size == count}
+
+    flat = factors.flatten.sort
+    return false unless flat == flat.uniq
+
+    true
+  end
+end
