209ddaa7f2c64d5f5d638ffa5687238048e7cc12
Patrick Elsen <pelsen@xfbs.net>
2018-01-17 19:40:34 +0100

    [032/ruby] adds half assed working code

--- src/032-pandigital-products/ruby/test/solver_test.rb
+++ src/032-pandigital-products/ruby/test/solver_test.rb
@@ -2,8 +2,8 @@ require 'minitest/autorun'
 require_relative '../src/solver'
 
 class SolverTest < MiniTest::Test
-  def test_solver
-    assert_equal Solver.solve(2), 6
-    assert_equal Solver.solve(4), 28
+  def test_pandigital_product_sums
+    assert_nil Solver.pandigital_product_sums((1..9).to_a)
+    assert_equal Solver.pandigital_product_sums([3, 9, 1, 8, 6, 7, 2, 5, 4]), [7254]
   end
 end
