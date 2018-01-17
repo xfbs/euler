9a130123e27cc5ee8349437067596c3923f644f7
Patrick Elsen <pelsen@xfbs.net>
2018-01-17 16:11:15 +0100

    [028/rust] adds implementation

---
+++ src/028-number-spiral-diagonals/rust/src/lib.rs
@@ -0,0 +1,19 @@
+pub fn solve(dim: u64) -> u64 {
+    let mut sum = 1u64;
+    let mut cur = 1u64;
+
+    for ring in (2..(dim+1)).filter(|n| n % 2 == 0) {
+        cur += ring; sum += cur;
+        cur += ring; sum += cur;
+        cur += ring; sum += cur;
+        cur += ring; sum += cur;
+    }
+
+    sum
+}
+
+#[test]
+fn test_solve() {
+    assert_eq!(solve(3), 25);
+    assert_eq!(solve(5), 101);
+}
