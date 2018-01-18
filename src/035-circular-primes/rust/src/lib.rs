0fe96beb5fe023af7b2003289ebfb607612ec9dc
Patrick Elsen <pelsen@xfbs.net>
2018-01-18 12:56:09 +0100

    [035/rust] adds solution

---
+++ src/035-circular-primes/rust/src/lib.rs
@@ -0,0 +1,29 @@
+extern crate euler;
+mod number_rotations;
+use euler::prime::Prime;
+use number_rotations::Rotatable;
+use std::collections::HashSet;
+
+pub fn solve(max: u64) -> usize {
+    let primes = Prime::new()
+        .into_iter()
+        .take_while(|n| *n < max)
+        .collect::<Vec<u64>>();
+
+    let mut rotatable = HashSet::new();
+
+    for prime in &primes {
+        if prime.rotations().all(|rot| primes.binary_search(&rot).is_ok()) {
+            for rot in prime.rotations() {
+                rotatable.insert(rot);
+            }
+        }
+    }
+
+    rotatable.iter().count()
+}
+
+#[test]
+fn test_solve() {
+    assert_eq!(solve(100), 13);
+}
