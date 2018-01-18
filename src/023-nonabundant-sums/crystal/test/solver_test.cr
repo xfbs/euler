c443415e58cedd66aaf9e1b97c663c57073d7f38
Patrick Elsen <pelsen@xfbs.net>
2018-01-18 09:22:06 +0100

    [023/crystal] adds tests

--- src/023-nonabundant-sums/crystal/test/solver_test.cr
+++ src/023-nonabundant-sums/crystal/test/solver_test.cr
@@ -2,6 +2,10 @@ require "minitest/autorun"
 require "../src/solver"
 
 class SolverTest < Minitest::Test
-  def test_solver
+  def test_solve
+    assert_equal Solver.solve(10), 45
+    assert_equal Solver.solve(20), 190
+    assert_equal Solver.solve(25), 276
+    assert_equal Solver.solve(32), 442
   end
 end
