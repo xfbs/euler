b95eff27498f1c7eb11ff6edf5469352ac780ca2
Patrick Elsen <pelsen@xfbs.net>
2018-01-18 09:22:27 +0100

    [023/rust] adds solution

---
+++ src/023-nonabundant-sums/rust/src/lib.rs
@@ -0,0 +1,36 @@
+extern crate euler;
+use euler::divisor_sum::*;
+
+pub fn solve(max: u64) -> u64 {
+    let mut possible = vec![false; max as usize];
+
+    let abundant = (1..max)
+        .filter(|n| divisor_sum(*n) > *n)
+        .collect::<Vec<u64>>();
+
+    for a in 0..abundant.len() {
+        for b in a..abundant.len() {
+            let sum = abundant[a] + abundant[b];
+
+            if sum >= max {
+                break;
+            }
+
+            possible[sum as usize] = true;
+        }
+    }
+
+    possible.iter()
+        .enumerate()
+        .filter(|&(_, poss)| !poss)
+        .map(|(index, _)| index as u64)
+        .sum()
+}
+
+#[test]
+fn test_solve() {
+    assert_eq!(solve(10), 45);
+    assert_eq!(solve(20), 190);
+    assert_eq!(solve(25), 276);
+    assert_eq!(solve(32), 442);
+}
