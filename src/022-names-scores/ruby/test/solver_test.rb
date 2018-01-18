92ae7921295ea19ac7252e5d91e0fde79db5a92b
Patrick Elsen <pelsen@xfbs.net>
2018-01-18 08:17:21 +0100

    [022/ruby] adds test

--- src/022-names-scores/ruby/test/solver_test.rb
+++ src/022-names-scores/ruby/test/solver_test.rb
@@ -2,6 +2,10 @@ require 'minitest/autorun'
 require_relative '../src/solver'
 
 class SolverTest < MiniTest::Test
+  def test_solve
+    assert_equal Solver.solve(["A", "Z", "COLIN"]), 1*1 + 2*53 + 3*26
+  end
+
   def test_weight
     assert_equal 1, Solver.weight("A")
     assert_equal 26, Solver.weight("Z")
