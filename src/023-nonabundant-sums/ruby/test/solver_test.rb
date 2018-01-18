7955ab72c0389693e98a33e883b1508113446322
Patrick Elsen <pelsen@xfbs.net>
2018-01-18 09:21:57 +0100

    [023/ruby] adds test

--- src/023-nonabundant-sums/ruby/test/solver_test.rb
+++ src/023-nonabundant-sums/ruby/test/solver_test.rb
@@ -2,4 +2,10 @@ require 'minitest/autorun'
 require_relative '../src/solver'
 
 class SolverTest < MiniTest::Test
+  def test_solve
+    assert_equal Solver.solve(10), 45
+    assert_equal Solver.solve(20), 190
+    assert_equal Solver.solve(25), 276
+    assert_equal Solver.solve(32), 442
+  end
 end
