6e999d9f9bf5cb531995939577d2e04c944d5c19
Patrick Elsen <pelsen@xfbs.net>
2018-01-19 17:35:14 +0100

    [027/c] adds more tests

--- src/027-quadratic-primes/c/test/solve_test.c
+++ src/027-quadratic-primes/c/test/solve_test.c
@@ -6,7 +6,10 @@ void test_quadratic_prime_run()
     prime_t p = prime_new();
 
     assert(40 == quadratic_prime_run(&p, 1, 41));
+    assert(41 == quadratic_prime_run(&p, -1, 41));
     assert(80 == quadratic_prime_run(&p, -79, 1601));
+    assert( 0 == quadratic_prime_run(&p, -120, 45));
+    assert( 2 == quadratic_prime_run(&p, 9, 13));
 
     prime_free(&p);
 }
@@ -14,6 +17,7 @@ void test_quadratic_prime_run()
 void test_solve()
 {
     assert(-126479 == solve(1700));
+    assert(-41 == solve(42));
 }
 
 TESTS() {
