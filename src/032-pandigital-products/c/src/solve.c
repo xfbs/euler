c2da8382ed90af15bee7fb65d9f214cd1ad5523c
Patrick Elsen <pelsen@xfbs.net>
2018-01-19 18:09:44 +0100

    [032/c] simplifies solution

--- src/032-pandigital-products/c/src/solve.c
+++ src/032-pandigital-products/c/src/solve.c
@@ -1,63 +1,52 @@
 #include <stdbool.h>
 #include <stdlib.h>
+#include <euler/vec16.h>
 #include "solve.h"
 
 uint32_t solve()
 {
-    size_t len = 256;
-    size_t pos = 0;
-    uint32_t *list = calloc(len, sizeof(uint32_t));
+    vec16_t products = vec16_new(0, 0);
 
-    for(uint32_t a = 1; a < 999; a++) {
+    for(uint32_t a = 1; a < 99; a++) {
         for(uint32_t b = a; b < 9999; b++) {
             uint32_t p = pandigital_product(a, b);
             if(p != 0) {
-                bool exists = false;
-                for(size_t i = 0; i < pos; i++) {
-                    if(list[i] == p) exists = true;
-                }
-
-                if(!exists) {
-                    list[pos] = p;
-                    pos++;
-                }
-
-                if(pos == len) {
-                    len *= 2;
-                    list = realloc(list, len * sizeof(uint32_t));
+                if(SIZE_MAX == vec16_index(&products, p)) {
+                    vec16_push(&products, p);
                 }
             }
         }
     }
 
-    uint32_t sum = 0;
-    for(size_t i = 0; i < pos; i++) {
-        sum += list[i];
-    }
-
-    return sum;
+    return vec16_sum(&products);
 }
 
 uint32_t pandigital_product(uint32_t a, uint32_t b)
 {
-    uint32_t nums = 0;
-    uint32_t s, sum = a * b;
-    s = sum;
+    bool digits[10] = {false};
+    uint32_t product = a * b;
+    uint32_t p = product;
 
     for( ; a > 0; a /= 10) {
-        if(nums & (1<<(a%10))) return 0;
-        nums |= (1<<(a%10));
+        if(digits[a % 10]) return 0;
+        digits[a % 10] = true;
     }
 
     for( ; b > 0; b /= 10) {
-        if(nums & (1<<(b%10))) return 0;
-        nums |= (1<<(b%10));
+        if(digits[b % 10]) return 0;
+        digits[b % 10] = true;
     }
 
-    for( ; s > 0; s /= 10) {
-        if(nums & (1<<(s%10))) return 0;
-        nums |= (1<<(s%10));
+    for( ; p > 0; p /= 10) {
+        if(digits[p % 10]) return 0;
+        digits[p % 10] = true;
     }
 
-    return (nums == ((1<<10) - 2)) ? sum : 0;
+    for(size_t digit = 0; digit < 10; digit++) {
+        if(digits[digit] == (digit == 0)) {
+            return 0;
+        }
+    }
+
+    return product;
 }
