3c7d10f02bbb5722b34a956b1fb28f50b03d6db7
Patrick Elsen <pelsen@xfbs.net>
2018-01-19 11:59:24 +0100

    [041/c] removes prime_check() function

--- src/041-pandigital-prime/c/src/solve.c
+++ src/041-pandigital-prime/c/src/solve.c
@@ -6,19 +6,6 @@
 #include "euler/pandigital.h"
 #include "solve.h"
 
-static bool prime_check(prime_t *p, uint32_t num)
-{
-    uint32_t root = sqrt(num);
-
-    for(size_t i = 0; prime_nth(p, i) < root; i++) {
-        if((num % prime_nth(p, i)) == 0) {
-            return false;
-        }
-    }
-
-    return true;
-}
-
 uint32_t solve(uint8_t max)
 {
     prime_t p = prime_new();
