7c8994be15c4d64e1cca8c823410b2a95831e2fe
Patrick Elsen <pelsen@xfbs.net>
2018-01-22 15:03:44 +0100

    [007/c] fixes for lib/c consolidation

--- src/007-10001st-prime/c/src/solve.c
+++ src/007-10001st-prime/c/src/solve.c
@@ -1,10 +1,14 @@
-#include <euler/simple_prime.h>
+#include <euler/prime.h>
 #include "solve.h"
 
 uint64_t solve(uint32_t pos)
 {
     prime_t p = prime_new();
-    uint64_t prime = prime_nth(&p, pos-1);
+
+    // generate nth prime (subtract pos by one because prime_nth is
+    // zero-indexed)
+    uint64_t prime = prime_nth(&p, pos - 1);
+
     prime_free(&p);
 
     return prime;
