6b5b3caae80907cf75f64dc484318f8090ef496b
Patrick Elsen <pelsen@xfbs.net>
2018-01-22 01:05:52 +0100

    [004/crystal] outsources palindrome check

--- src/004-largest-palindrome-product/crystal/src/solver.cr
+++ src/004-largest-palindrome-product/crystal/src/solver.cr
@@ -1,25 +1,15 @@
+require "euler/palindrome"
+
 module Solver
   def self.solve(digits)
     min = 10**(digits-1)
     max = 10**digits
     (min...max).map do |a|
       (a...max).select do |b|
-        is_palindrome?(a * b)
+        Euler.is_palindrome?(a * b)
       end.map do |b|
         a * b
-      end.push(0).max
+      end.max? || 0
     end.max
-  end
-
-  def self.is_palindrome?(num)
-    cur = num
-    rev = 0
-    while cur > 0
-      rev *= 10
-      rev += (cur % 10)
-      cur /= 10
-    end
-
-    rev == num
   end
 end
--- src/004-largest-palindrome-product/crystal/test/solver_test.cr
+++ src/004-largest-palindrome-product/crystal/test/solver_test.cr
@@ -2,16 +2,4 @@ require "minitest/autorun"
 require "../src/solver"
 
 class SolverTest < Minitest::Test
-  def test_is_palindrome?
-    assert Solver.is_palindrome?(101)
-    assert Solver.is_palindrome?(163361)
-    assert Solver.is_palindrome?(94249)
-    assert Solver.is_palindrome?(3301033)
-    assert Solver.is_palindrome?(19291)
-
-    assert !Solver.is_palindrome?(19281)
-    assert !Solver.is_palindrome?(123)
-    assert !Solver.is_palindrome?(4923)
-    assert !Solver.is_palindrome?(2439)
-  end
 end
