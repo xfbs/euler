81e92265f77582de7aee1909755c2d54d96d6c99
Patrick Elsen <pelsen@xfbs.net>
2018-01-22 21:56:39 +0100

    [lib/rust] adds divisors

--- lib/rust/euler/src/divisors.rs
+++ lib/rust/euler/src/divisors.rs
@@ -1,3 +1,5 @@
+use std::ops::{Add, AddAssign, Div, DivAssign, Mul, MulAssign, Rem, RemAssign};
+
 pub fn divisor_sum(num: u64) -> u64 {
     let root = (num as f64).sqrt() as u64;
     let mut sum = 0;
@@ -32,4 +34,74 @@ fn test_divisor_sum() {
         divisor_sum(60),
         1 + 2 + 3 + 4 + 5 + 6 + 10 + 12 + 15 + 20 + 30
     );
+}
+
+#[derive(Debug, Copy, Clone)]
+pub struct Divisors<T> {
+    num: T,
+    cur: T,
+    chk: bool,
+}
+
+impl<T> Iterator for Divisors<T>
+where
+    T: Copy
+        + AddAssign
+        + Div<Output = T>
+        + Rem<Output = T>
+        + Mul<Output = T>
+        + PartialOrd
+        + From<u8>,
+{
+    type Item = T;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        if self.cur * self.cur > self.num {
+            None
+        } else {
+            if !self.chk {
+                if self.num % self.cur == 0u8.into() {
+                    self.chk = true;
+                    Some(self.cur)
+                } else {
+                    self.cur += 1u8.into();
+                    self.next()
+                }
+            } else {
+                self.chk = false;
+                let recp = self.num / self.cur;
+
+                if self.cur != recp && self.cur != 1u8.into() {
+                    self.cur += 1u8.into();
+                    Some(recp)
+                } else {
+                    self.cur += 1u8.into();
+                    self.next()
+                }
+            }
+        }
+    }
+}
+
+pub trait ToDivisors where Self: Sized {
+    fn divisors(&self) -> Divisors<Self>;
+}
+
+impl<T> ToDivisors for T
+where
+    T: Copy + From<u8>,
+{
+    fn divisors(&self) -> Divisors<Self> {
+        Divisors {
+            num: *self,
+            cur: 1u8.into(),
+            chk: false
+        }
+    }
+}
+
+#[test]
+fn test_divisors() {
+    assert_eq!(220.divisors().sum::<i32>(), 284);
+    assert_eq!(284.divisors().sum::<i32>(), 220);
 }
