9e7341d07328e9c094743b59152f3b7e7d2e0c09
Patrick Elsen <pelsen@xfbs.net>
2018-01-22 01:05:29 +0100

    [002/crystal] outsources fibonacci function

--- src/002-even-fibonacci-numbers/crystal/src/solver.cr
+++ src/002-even-fibonacci-numbers/crystal/src/solver.cr
@@ -1,10 +1,10 @@
-require "./fibonacci"
+require "euler/fibonacci"
 
 module Solver
   def self.solve(n)
     (1..n)
       .each
-      .map{|i| Fibonacci.nth(3*i).to_u64}
+      .map{|i| Euler.fibonacci(3*i).to_u64}
       .take_while{|i| i < n}
       .sum
   end
