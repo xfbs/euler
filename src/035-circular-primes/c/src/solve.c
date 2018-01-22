2378cfc76c8c5fca515cee4dc5c9b3122e1e9fcb
Patrick Elsen <pelsen@xfbs.net>
2018-01-22 15:15:05 +0100

    [035/c] renames primes_below() -> prime_below()

--- src/035-circular-primes/c/src/solve.c
+++ src/035-circular-primes/c/src/solve.c
@@ -4,7 +4,7 @@ uint32_t solve(size_t max)
 {
     // generate all primes below max
     prime_t p = prime_new();
-    size_t primes = primes_below(&p, max);
+    size_t primes = prime_below(&p, max);
 
     // bitvec to store the cyclic primes we found
     bitvec_t b = bitvec_new(primes);
