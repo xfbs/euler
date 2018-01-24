df61888a46824660d8095932eb9b035c57839212
Patrick Elsen <pelsen@xfbs.net>
2018-01-24 17:40:08 +0100

    [051/ocaml] fixes makefile

--- src/051-prime-digit-replacements/ocaml/test/test.ml
+++ src/051-prime-digit-replacements/ocaml/test/test.ml
@@ -2,7 +2,7 @@ open OUnit
 open Awesome
 
 let suite = "OUnit tests..." >:::  ["test_list_length" >::
-                                    (fun () -> assert_equal "1" (str_of_t (succ one_t)))]
+                                    (fun () -> assert_equal "2" (str_of_t (succ one_t)))]
 
 let _ = 
   run_test_tt_main suite
