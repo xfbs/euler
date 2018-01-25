9b136ca199cec0c0acf8f596afde115f79275c68
Patrick Elsen <pelsen@xfbs.net>
2018-01-25 13:56:07 +0100

    [lib/cpp] half-baked primes implementation in cpp

--- lib/cpp/src/prime.cpp
+++ lib/cpp/src/prime.cpp
@@ -1,7 +1,32 @@
 #include "euler/prime.hpp"
+#include <cmath>
 
 using namespace euler;
 
 Prime::Prime() {
+    primes = {2, 3};
 }
 
+uint64_t Prime::nth(size_t pos) {
+    while(primes.size() <= pos) next();
+    return primes[pos];
+}
+
+uint64_t Prime::next() {
+    uint64_t next = primes[primes.size() - 1] + 2;
+    while(!check(next)) next += 2;
+    primes.push_back(next);
+    return next;
+}
+
+bool Prime::check(uint64_t prime) {
+    uint64_t sqrt = std::sqrt(prime);
+
+    for(size_t i = 0; primes[i] <= sqrt; i++) {
+        if(prime % primes[i] == 0) {
+            return false;
+        }
+    }
+
+    return true;
+}
--- lib/cpp/test/prime_test.cpp
+++ lib/cpp/test/prime_test.cpp
@@ -1,1 +1,26 @@
+#include <catch/catch.hpp>
 #include <euler/prime.hpp>
+
+TEST_CASE("primes can be allocated", "[Prime]") {
+    auto primes = euler::Prime();
+}
+
+TEST_CASE("can return any given prime", "[Prime]") {
+    auto primes = euler::Prime();
+
+    SECTION("initial primes are available") {
+        REQUIRE(primes.nth(0) == 2);
+        REQUIRE(primes.nth(1) == 3);
+    }
+
+    SECTION("further primes can be generated") {
+        REQUIRE(primes.nth(2) == 5);
+        REQUIRE(primes.nth(3) == 7);
+    }
+
+    SECTION("generation does not need to be sequential") {
+        REQUIRE(primes.nth(7) == 19);
+        REQUIRE(primes.nth(10) == 31);
+        REQUIRE(primes.nth(5) == 13);
+    }
+}
