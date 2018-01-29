c560330239c43e00a31a50c2c1b0b5cb615dd473
Patrick Elsen <pelsen@xfbs.net>
2018-01-29 19:03:43 +0100

    [051/cpp] got a working solver, need to clean up stuff

--- src/051-prime-digit-replacements/cpp/bin/range.cpp
+++ src/051-prime-digit-replacements/cpp/bin/range.cpp
@@ -1,7 +1,7 @@
-#include <range/v3/all.hpp>
-#include <vector>
 #include <array>
 #include <iostream>
+#include <range/v3/all.hpp>
+#include <vector>
 
 using std::cout;
 using std::endl;
@@ -11,23 +11,25 @@ auto is_six = [](int i) -> bool { return i == 6; };
 auto doub = [](int i) -> int { return 2 * i; };
 
 int main() {
-  std::vector<int> v { 6, 2, 3, 4, 5, 6 };
-  auto c = count_if( v, is_six );
+  std::vector<int> v{6, 2, 3, 4, 5, 6};
+  auto c = count_if(v, is_six);
   cout << "count-sixes: " << c << endl;
 
-  auto s = accumulate( v, 0 );
+  auto s = accumulate(v, 0);
   cout << "sum-vetor: " << s << endl;
 
   auto cand = view::ints(1) | view::transform(doub);
-  auto e = find_if( cand, is_six );
+  auto e = find_if(cand, is_six);
   cout << "find-six: " << *e << endl;
 
-  auto odd_perfect_square = front(
-    view::ints
-    | view::transform([](int a){return a * a;}) // make perfect squares
-    | view::filter([](int a){return a % 2 == 1;}) // keep odd ones
-    | view::drop_exactly(2)); // we don't care about 1 and 9
+  auto odd_perfect_square =
+      front(view::ints
+            // make perfect squares
+            | view::transform([](int a) { return a * a; })
+            // keep odd ones only
+            | view::filter([](int a) { return a % 2 == 1; })
+            // ignore first two
+            | view::drop_exactly(2));
 
   cout << odd_perfect_square << endl;
-
 }
--- src/051-prime-digit-replacements/cpp/src/solve.cpp
+++ src/051-prime-digit-replacements/cpp/src/solve.cpp
@@ -1,24 +1,62 @@
 #include "solve.hpp"
-#include <range/v3/all.hpp>
 #include <euler/prime.hpp>
+#include <euler/digits.hpp>
+#include <range/v3/all.hpp>
 using namespace ranges;
 using namespace euler;
+
+uint8_t family_size(Prime &primes, uint64_t prime, uint64_t digit) {
+  auto not_cur_digit = [digit](uint64_t candidate) {
+    return digit != candidate;
+  };
+
+  auto is_prime = [&primes](uint64_t num) {
+    return primes.check(num);
+  };
+
+  auto replace_digit_with = [prime, digit](uint64_t replacement) {
+    auto replace_digit = [digit, replacement](uint64_t cur) {
+      if(cur == digit) {
+        return replacement;
+      } else {
+        return cur;
+      }
+    };
+
+    auto transformed = digits(prime) | view::transform(replace_digit);
+    return accumulate(transformed, 0, [](uint64_t memo, uint64_t cur) {
+        return 10 * memo + cur;
+    });
+  };
+
+  auto members =
+    view::ints(1, 10)
+    | view::filter(not_cur_digit)
+    | view::transform(replace_digit_with);
+
+  return 1 + count_if(members, is_prime);
+}
+
+bool check_family(Prime &primes, uint64_t prime, uint8_t family) {
+  auto is_family = [&primes, prime, family](uint64_t digit) {
+    return family == family_size(primes, prime, digit);
+  };
+
+  // FIXME: return early on success?
+  return count_if(reverse_digits(prime), is_family) > 0;
+}
 
 uint64_t solve(uint8_t family) {
   auto primes = Prime();
 
-  auto to_prime = [&primes](int nth) -> uint64_t {
-    return primes.nth(nth);
-  };
+  auto to_prime = [&primes](int nth) -> uint64_t { return primes.nth(nth); };
 
-  auto has_family = [](uint64_t prime) -> bool {
-    return true;
+  auto has_family = [&primes, family](uint64_t prime) -> bool {
+    return check_family(primes, prime, family);
   };
 
   auto candidates =
-    view::ints
-    | view::transform(to_prime)
-    | view::filter(has_family);
+      view::ints | view::transform(to_prime) | view::filter(has_family);
 
   return front(candidates);
 }
