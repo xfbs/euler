a611570d19220794d8a7f0a4261f241b465da15f
Patrick Elsen <pelsen@xfbs.net>
2018-01-30 12:38:27 +0100

    [041/crystal] simplifies solution

--- src/041-pandigital-prime/crystal/src/solver.cr
+++ src/041-pandigital-prime/crystal/src/solver.cr
@@ -9,27 +9,12 @@ module Solver
     num.step(to: 1, by: -1).each do |n|
       Euler.factorial64(n).step(to: 1, by: -1) do |nth|
         permutation = Euler.pandigital(n, nth - 1)
-        if check?(primes, permutation)
+        if primes.check? permutation
           return permutation
         end
       end
     end
 
     0
-  end
-
-  def self.check?(primes, prime)
-    root = Math.sqrt(prime).to_u64
-    cur = 0
-    div = primes.nth(cur)
-    while div <= root
-      if (prime % div) == 0
-        return false
-      end
-      cur += 1
-      div = primes.nth(cur)
-    end
-
-    true
   end
 end
