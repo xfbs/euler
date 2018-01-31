72dd2e7a5ec329ca21cec7169932cbcd6753c95b
Patrick Elsen <pelsen@xfbs.net>
2018-01-31 22:34:07 +0100

    [lib/c] adds documentation for test.h

--- lib/c/include/euler/test.h
+++ lib/c/include/euler/test.h
@@ -1,12 +1,60 @@
+//! @file test.h
+//! @author Patrick Elsen
+//! @brief tiny C unit testing helpers
 #include <assert.h>
 #include <stdio.h>
+
+//! @defgroup test test
+//! @brief tiny C unit testing helpers
+//! @{
+
+//! define the main testing function.
+//!
+//! this function may only be called once, and with it you define a function
+//! that will run all the tests that you specify.
+//!
+//! inside the function, call RUN() to run the tests you want to run.
+//!
+//! ## Examples
+//!
+//! ```c
+//! void test_something() {
+//!   assert(5 == 5);
+//!   assert(25 == (5 * 5));
+//! }
+//!
+//! TESTS() {
+//!   RUN(test_something);
+//! }
+//! ```
+#define TESTS()                                                                \
+  const char *_get_file_name() { return __FILE__; }                            \
+  void _tests()
+
+//! run a single test function with the provided name
+//!
+//! this function will run the given function and prime some helpful messages to
+//! stdout.
+//!
+//! this function is meant to be called in a TESTS() block.
+//!
+//! @param t function to test, must be a `void` function.
+//!
+//! ## Examples
+//!
+//! ```c
+//! void test_something() {
+//!   assert(true);
+//! }
+//!
+//! TESTS() {
+//!   RUN(test_something);
+//! }
+//! ```
 #define RUN(t)                                                                 \
   printf("  - %s: ", #t);                                                      \
   t();                                                                         \
   printf("success.\n")
-#define TESTS()                                                                \
-  const char *_get_file_name() { return __FILE__; }                            \
-  void _tests()
 
 void _tests();
 const char *_get_file_name();
@@ -16,3 +64,5 @@ int main(int argc, char *argv[]) {
   _tests();
   return 0;
 }
+
+//! @}
