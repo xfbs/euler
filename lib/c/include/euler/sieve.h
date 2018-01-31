ce5467a2a2f3552ea5d03c4d100a9e0afa27252f
Patrick Elsen <pelsen@xfbs.net>
2018-01-31 22:55:46 +0100

    [lib/c] adds rudimentary comments for sieve.h

--- lib/c/include/euler/sieve.h
+++ lib/c/include/euler/sieve.h
@@ -1,7 +1,19 @@
+//! @file prime.h
+//! @author Patrick M. Elsen
+//! @date 2018
+//! @copyright MIT license
+//! @brief functions for sieving for prime numbers.
+//! @detail these functions provide an easy interface for sieving for prime
+//! numbers.
+//! @todo incorporate into prime.h
 #include <stdbool.h>
 #include <stddef.h>
 #include <stdint.h>
 #pragma once
+
+//! @defgroup sieve sieve
+//! @brief functions for sieveing for prime numbers.
+//! @{
 
 typedef struct {
   bool *state;
@@ -14,3 +26,5 @@ uint32_t sieve_nth(sieve_t *p, size_t n);
 size_t sieve_which(sieve_t *p, uint32_t pr);
 bool sieve_check(sieve_t *p, uint32_t num);
 size_t sieve_below(sieve_t *p, uint32_t n);
+
+//! @}
