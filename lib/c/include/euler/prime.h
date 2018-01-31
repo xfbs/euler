ff491447bb3668545d6a2840da7321b4e4fb858d
Patrick Elsen <pelsen@xfbs.net>
2018-01-31 22:26:06 +0100

    [lib/c] finish documenting prime.h

--- lib/c/include/euler/prime.h
+++ lib/c/include/euler/prime.h
@@ -43,10 +43,119 @@ typedef struct {
 //! ```
 prime_t prime_new();
 
+//! free the contents of the `prime_t`.
+//!
+//!
+//! @param p the primes struct to free
+//! @warning
+//! this method only frees the contents of the `prime_t`, not the `prime_t`
+//! itself. if it was allocated on the heap, it must be `free()`d also.
+//!
+//! ## Examples
+//!
+//! ```c
+//! // new prime instance
+//! prime_t primes = prime_new();
+//!
+//! // free instance
+//! prime_free(&primes);
+//! ```
 void prime_free(prime_t *p);
+
+//! generate the nth prime.
+//!
+//! this method generates all primes up to the `n`th, and then returns the
+//! `n`th. counting starts at 0, so the 0th prime is 2. all primes (also
+//! intermediate primes) are cached for future lookup.
+//!
+//! @param p the prime generation instance to use
+//! @param n which prime to generate
+//! @return the `n`th prime
+//!
+//! ## Examples
+//!
+//! ```c
+//! // new prime generation instance
+//! prime_t primes = prime_new();
+//!
+//! assert(prime_nth(&primes, 0) == 2);
+//! assert(prime_nth(&primes, 1) == 3);
+//! assert(prime_nth(&primes, 2) == 5);
+//! assert(prime_nth(&primes, 3) == 7);
+//! assert(prime_nth(&primes, 25) > 100);
+//!
+//! // free instance
+//! prime_free(&primes);
+//! ```
 uint32_t prime_nth(prime_t *p, size_t n);
+
+//! reverse lookup for primes.
+//!
+//! given a prime `pr`, find which prime it is.
+//!
+//! @param p prime generation instance to use
+//! @param pr prime to look up
+//! @return position of prime or `SIZE_MAX`
+//!
+//! ## Examples
+//!
+//! ```c
+//! // new prime generation instance
+//! prime_t primes = prime_new();
+//!
+//! // test a few primes
+//! assert(prime_which(&primes, 7) == 3);
+//! assert(prime_which(&primes, 19) == 7);
+//! assert(prime_which(&primes, 10) == SIZE_MAX);
+//!
+//! // free instance
+//! prime_free(&primes);
+//! ```
 size_t prime_which(prime_t *p, uint32_t pr);
+
+//! check if a number is prime
+//!
+//! checks if the number is prime by testing it for divisibily by all primes up
+//! to `sqrt(num)`.
+//!
+//! @param p prime generation instance to use
+//! @param num number to check
+//! @return `true` if the number is prime, `false` otherwise
+//!
+//! ## Examples
+//!
+//! ```c
+//! // new prime generation instance
+//! prime_t primes = prime_new();
+//!
+//! // test a few primes
+//! assert(prime_check(&primes, 2) == true);
+//! assert(prime_check(&primes, 5) == true);
+//! assert(prime_check(&primes, 8) == false);
+//! assert(prime_check(&primes, 9) == false);
+//!
+//! // free instance
+//! prime_free(&primes);
+//! ```
 bool prime_check(prime_t *p, uint32_t num);
+
+//! generates all primes below `n` and returns how many there are.
+//!
+//! @param p prime generation instance to use
+//! @param n maximum of when to generate primes
+//!
+//! ## Examples
+//!
+//! ```c
+//! // new prime generation instance
+//! prime_t primes = prime_new();
+//!
+//! // generate primes up to 100
+//! assert(prime_below(&primes, 100) == 25);
+//!
+//! // free prime generation instance
+//! prime_free(&primes);
+//! ```
 size_t prime_below(prime_t *p, uint32_t n);
 
 //! @}
