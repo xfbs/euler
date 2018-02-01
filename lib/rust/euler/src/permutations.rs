2d0d7422c66da973adf33acfc951cadb9bca4770
Patrick Elsen <pelsen@xfbs.net>
2018-02-01 16:26:32 +0100

    [lib/rust] documents permutations

--- lib/rust/euler/src/permutations.rs
+++ lib/rust/euler/src/permutations.rs
@@ -1,9 +1,49 @@
 use math::factorial;
 
+/// An iterator over the permutation of elements.
 pub struct Permutations {
     num: u8,
     req: u64,
     cur: [bool; 20]
+}
+
+impl Permutations {
+    /// Returns an iterator over the `nth` permutation of `count` elements.
+    ///
+    /// Given a count (which must be lower than 20) and `nth`, returns an
+    /// iterator of length `count`, that for each position of the `nth`
+    /// permutations yields which element of the original would be at that
+    /// position.
+    ///
+    /// So for example, if the original was `[1, 2, 3, 4]` and that permutation
+    /// was `[3, 1, 4, 2]`, the iterator would return `[2, 0, 3, 1]` for that
+    /// permutation.
+    ///
+    /// You'll probably want to use `euler::permutations()` to call this
+    /// function, which is more concise.
+    ///
+    /// ## Examples
+    ///
+    /// To get the `nth` permutation of an array that is `len` long, just do
+    /// this:
+    ///
+    /// ```
+    /// use euler::Permutations;
+    ///
+    /// let original = [1290, 2132, 3901, 4881, 5893];
+    /// let nth = 94;
+    /// let permuted = Permutations::of(original.len(), nth)
+    ///     .map(|i| original[i])
+    ///     .collect::<Vec<u32>>();
+    /// assert_eq!(permuted, vec![4881, 5893, 3901, 1290, 2132]);
+    /// ```
+    pub fn of(count: usize, nth: usize) -> Self {
+        Permutations {
+            num: count as u8,
+            req: nth as u64,
+            cur: [false; 20]
+        }
+    }
 }
 
 impl Iterator for Permutations {
@@ -29,12 +69,24 @@ impl Iterator for Permutations {
     }
 }
 
-pub fn permutations(count: usize, req: usize) -> Permutations {
-    Permutations {
-        num: count as u8,
-        req: req as u64,
-        cur: [false; 20]
-    }
+/// Returns an iterator over the `nth` permutation of `count` elements.
+///
+/// See `Permutations::of()` for more information.
+///
+/// ## Examples
+///
+/// ```
+/// use euler::permutations;
+///
+/// let elements = vec![2, 4, 7, 8, 0, 1];
+/// let nth = 56;
+/// let permuted = permutations(elements.len(), nth)
+///     .map(|pos| elements[pos])
+///     .collect::<Vec<u32>>();
+/// assert_eq!(permuted, vec![2, 8, 7, 0, 4, 1]);
+/// ```
+pub fn permutations(count: usize, nth: usize) -> Permutations {
+    Permutations::of(count, nth)
 }
 
 #[test]
