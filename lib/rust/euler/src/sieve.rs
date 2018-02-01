86507fa01d12e3088012dc7edef7a17e57da99ee
Patrick Elsen <pelsen@xfbs.net>
2018-02-01 16:05:38 +0100

    [lib/rust] documents prime sieve

--- lib/rust/euler/src/sieve.rs
+++ lib/rust/euler/src/sieve.rs
@@ -1,11 +1,38 @@
 use std::vec::IntoIter;
 use std::iter::{Enumerate, FilterMap};
 
+/// Prime sieve.
+///
+/// Implements `IntoIter` so that it can be used as an iterator over the primes.
+///
+/// ## Examples
+///
+/// ```
+/// use euler::Sieve;
+///
+/// let sieve = Sieve::new(100);
+/// assert_eq!(sieve.into_iter().count(), 25);
+/// ```
 pub struct Sieve {
     state: Vec<bool>,
 }
 
 impl Sieve {
+    /// Create a new prime sieve, and sieve all numbers up to `max`.
+    ///
+    /// Sieves all numbers from 0 until `max` and returns a `Sieve` instance
+    /// with the resulting matrix.
+    ///
+    /// ## Examples
+    ///
+    /// ```
+    /// use euler::Sieve;
+    ///
+    /// let sieve = Sieve::new(1000);
+    /// assert_eq!(sieve.check(17), Some(true));
+    /// assert_eq!(sieve.check(20), Some(false));
+    /// assert_eq!(sieve.check(1001), None);
+    /// ```
     pub fn new(max: usize) -> Self {
         let mut state = vec![true; max + 1];
         state[0] = false;
@@ -26,8 +53,20 @@ impl Sieve {
         Sieve { state: state }
     }
 
-    pub fn check(&self, num: u64) -> bool {
-        self.state[num as usize]
+    /// Checks if a given number is prime or not.
+    ///
+    /// ## Examples
+    ///
+    /// ```
+    /// use euler::Sieve;
+    ///
+    /// let sieve = Sieve::new(1000);
+    /// assert_eq!(sieve.check(17), Some(true));
+    /// assert_eq!(sieve.check(20), Some(false));
+    /// assert_eq!(sieve.check(1001), None);
+    /// ```
+    pub fn check(&self, num: u64) -> Option<bool> {
+        self.state.get(num as usize).cloned()
     }
 
     fn filter_prime((pos, prime): (usize, bool)) -> Option<u64> {
@@ -55,39 +94,42 @@ impl IntoIterator for Sieve {
 fn test_sieve_check_small() {
     let sieve = Sieve::new(10);
 
-    assert!(sieve.check(2));
-    assert!(sieve.check(3));
-    assert!(sieve.check(5));
-    assert!(sieve.check(7));
+    assert!(sieve.check(2).unwrap());
+    assert!(sieve.check(3).unwrap());
+    assert!(sieve.check(5).unwrap());
+    assert!(sieve.check(7).unwrap());
 
-    assert!(!sieve.check(0));
-    assert!(!sieve.check(1));
-    assert!(!sieve.check(4));
-    assert!(!sieve.check(6));
-    assert!(!sieve.check(8));
-    assert!(!sieve.check(9));
-    assert!(!sieve.check(10));
+    assert!(!sieve.check(0).unwrap());
+    assert!(!sieve.check(1).unwrap());
+    assert!(!sieve.check(4).unwrap());
+    assert!(!sieve.check(6).unwrap());
+    assert!(!sieve.check(8).unwrap());
+    assert!(!sieve.check(9).unwrap());
+    assert!(!sieve.check(10).unwrap());
+    assert!(sieve.check(11).is_none());
 }
 
 #[test]
 fn test_sieve_check_big() {
     let sieve = Sieve::new(100);
 
-    assert!(sieve.check(2));
-    assert!(sieve.check(3));
-    assert!(sieve.check(5));
-    assert!(sieve.check(7));
-    assert!(sieve.check(41));
-    assert!(sieve.check(47));
+    assert!(sieve.check(2).unwrap());
+    assert!(sieve.check(3).unwrap());
+    assert!(sieve.check(5).unwrap());
+    assert!(sieve.check(7).unwrap());
+    assert!(sieve.check(41).unwrap());
+    assert!(sieve.check(47).unwrap());
 
-    assert!(!sieve.check(0));
-    assert!(!sieve.check(10));
-    assert!(!sieve.check(98));
-    assert!(!sieve.check(60));
-    assert!(!sieve.check(44));
-    assert!(!sieve.check(25));
-    assert!(!sieve.check(90));
-    assert!(!sieve.check(55));
+    assert!(!sieve.check(0).unwrap());
+    assert!(!sieve.check(10).unwrap());
+    assert!(!sieve.check(98).unwrap());
+    assert!(!sieve.check(60).unwrap());
+    assert!(!sieve.check(44).unwrap());
+    assert!(!sieve.check(25).unwrap());
+    assert!(!sieve.check(90).unwrap());
+    assert!(!sieve.check(55).unwrap());
+
+    assert!(sieve.check(101).is_none());
 }
 
 #[test]
