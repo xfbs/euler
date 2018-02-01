9b0514899b569ae9da133beb0756942878093ba6
Patrick Elsen <pelsen@xfbs.net>
2018-02-01 18:34:38 +0100

    [lib/c] adds euler/common.h for common includes and euler.h for all

---
+++ lib/c/include/euler.h
@@ -0,0 +1,10 @@
+//! @file euler.h
+//! @brief loads all headers
+#include <euler/common.h>
+#include <euler/vec.h>
+#include <euler/math.h>
+#include <euler/collatz.h>
+#include <euler/modular_arithmetic.h>
+#include <euler/prime.h>
+#include <euler/sieve.h>
+#pragma once
---
+++ lib/c/include/euler/common.h
@@ -0,0 +1,6 @@
+//! @file common.h
+//! @brief loads common headers used by many solutions
+#include <stdint.h>
+#include <stddef.h>
+#include <stdbool.h>
+#pragma once
