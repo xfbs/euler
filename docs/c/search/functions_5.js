9ff7cc3543453793191ccaa4ec52386a539d2b08
Patrick Elsen <pelsen@xfbs.net>
2018-02-06 13:30:28 +0100

    updates documentation

--- docs/c/search/functions_5.js
+++ docs/c/search/functions_5.js
@@ -1,8 +1,9 @@
 var searchData=
 [
   ['map_5fadd',['map_add',['../group__map.html#ga2839977e34a5fbf56f5821a67d962a8c',1,'map_add(map_t *m, const char *str, void *val):&#160;map.c'],['../group__map.html#ga2839977e34a5fbf56f5821a67d962a8c',1,'map_add(map_t *m, const char *str, void *val):&#160;map.c']]],
-  ['map_5fcmp_5fptr',['map_cmp_ptr',['../group__map.html#ga3a34fe6a93edac3effb6dc5df40c00a6',1,'map.h']]],
-  ['map_5fcmp_5fstr',['map_cmp_str',['../group__map.html#ga618b9485462ce34d1297f00b7a94043c',1,'map.h']]],
+  ['map_5falloc',['map_alloc',['../group__map.html#ga4f22faffa1316f06f0c81e4f46dc8d78',1,'map_alloc():&#160;map.c'],['../group__map.html#ga4f22faffa1316f06f0c81e4f46dc8d78',1,'map_alloc():&#160;map.c']]],
+  ['map_5fcmp_5fptr',['map_cmp_ptr',['../group__map.html#ga3a34fe6a93edac3effb6dc5df40c00a6',1,'map_cmp_ptr(const void *lhs, const void *rhs):&#160;map.c'],['../group__map.html#ga3a34fe6a93edac3effb6dc5df40c00a6',1,'map_cmp_ptr(const void *lhs, const void *rhs):&#160;map.c']]],
+  ['map_5fcmp_5fstr',['map_cmp_str',['../group__map.html#ga618b9485462ce34d1297f00b7a94043c',1,'map_cmp_str(const void *lhs, const void *rhs):&#160;map.c'],['../group__map.html#ga618b9485462ce34d1297f00b7a94043c',1,'map_cmp_str(const void *lhs, const void *rhs):&#160;map.c']]],
   ['map_5fdel',['map_del',['../group__map.html#ga66b79711a646177f127f92109df5a13b',1,'map_del(map_t *m, const void *key):&#160;map.c'],['../group__map.html#ga66b79711a646177f127f92109df5a13b',1,'map_del(map_t *m, const void *key):&#160;map.c']]],
   ['map_5ffree',['map_free',['../group__map.html#ga316fe7ee6defa6a4fe2714d50fce875a',1,'map_free(map_t *hm):&#160;map.c'],['../group__map.html#ga316fe7ee6defa6a4fe2714d50fce875a',1,'map_free(map_t *hm):&#160;map.c']]],
   ['map_5fget',['map_get',['../group__map.html#gacb670408efab812020515b30a663c07d',1,'map_get(const map_t *m, const char *str):&#160;map.c'],['../group__map.html#gacb670408efab812020515b30a663c07d',1,'map_get(const map_t *m, const char *str):&#160;map.c']]],
@@ -11,8 +12,8 @@ var searchData=
   ['map_5fhash_5fstr',['map_hash_str',['../group__map.html#gae9008c161311a0559e9f73287162f146',1,'map_hash_str(const char *str):&#160;map.c'],['../group__map.html#gae9008c161311a0559e9f73287162f146',1,'map_hash_str(const char *str):&#160;map.c']]],
   ['map_5flen',['map_len',['../group__map.html#gae6ffd417d52ddd844a90b2478ba530c5',1,'map_len(const map_t *m):&#160;map.c'],['../group__map.html#gae6ffd417d52ddd844a90b2478ba530c5',1,'map_len(const map_t *hm):&#160;map.c']]],
   ['map_5fnew',['map_new',['../group__map.html#gaad87982635013ef186637e190ecfe189',1,'map_new():&#160;map.c'],['../group__map.html#gaad87982635013ef186637e190ecfe189',1,'map_new():&#160;map.c']]],
-  ['map_5fset',['map_set',['../group__map.html#ga1aa759d70ec9c7447448bf6379b36253',1,'map_set(map_t *m, const char *key, void *val):&#160;map.c'],['../group__map.html#ga1aa759d70ec9c7447448bf6379b36253',1,'map_set(map_t *m, const char *str, void *val):&#160;map.c']]],
-  ['map_5fset_5fcmp',['map_set_cmp',['../group__map.html#ga2a913fcc229b0a86560aa4fe54e9e7a8',1,'map.h']]],
+  ['map_5fset',['map_set',['../group__map.html#ga1aa759d70ec9c7447448bf6379b36253',1,'map_set(map_t *m, const char *key, void *val):&#160;map.c'],['../group__map.html#ga1aa759d70ec9c7447448bf6379b36253',1,'map_set(map_t *m, const char *ptr, void *val):&#160;map.c']]],
+  ['map_5fset_5fcmp',['map_set_cmp',['../group__map.html#ga2a913fcc229b0a86560aa4fe54e9e7a8',1,'map_set_cmp(map_t *hm, map_cmp_fn *cmp):&#160;map.c'],['../group__map.html#ga2a913fcc229b0a86560aa4fe54e9e7a8',1,'map_set_cmp(map_t *hm, map_cmp_fn *cmp):&#160;map.c']]],
   ['map_5fset_5ffree',['map_set_free',['../group__map.html#ga04de750727417868b2bdfb741fc84925',1,'map_set_free(map_t *hm, map_free_fn *free_key, map_free_fn *free_val):&#160;map.c'],['../group__map.html#ga04de750727417868b2bdfb741fc84925',1,'map_set_free(map_t *hm, map_free_fn *free_key, map_free_fn *free_val):&#160;map.c']]],
   ['map_5fset_5fhash',['map_set_hash',['../group__map.html#ga28a9be3dce6986c8926ebfc425724390',1,'map_set_hash(map_t *hm, map_hash_fn *hash):&#160;map.c'],['../group__map.html#ga28a9be3dce6986c8926ebfc425724390',1,'map_set_hash(map_t *hm, map_hash_fn *hash):&#160;map.c']]],
   ['mult_5fmod',['mult_mod',['../group__modarith.html#ga1bf6cd7ddff609d214b7e4d4ea188f1e',1,'mult_mod(uint64_t a, uint64_t b, uint64_t mod):&#160;modarith.c'],['../group__modarith.html#ga1bf6cd7ddff609d214b7e4d4ea188f1e',1,'mult_mod(uint64_t a, uint64_t b, uint64_t mod):&#160;modarith.c']]]
